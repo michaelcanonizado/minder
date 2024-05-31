d95f7947475d89b7dd83f3db8e5399103a38a5aa
unknown <michaelxaviercanonizado@gmail.com>
2024-05-31 19:58:13 +0800

    feat: add Wallets component

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,14 +1,39 @@
 import Balance from '@/components/sections/demo/balance';
+import Wallets from '@/components/sections/demo/wallets';
 
 export default function Demo() {
-  console.log('Hello World');
-
   return (
-    <div className='mt-20 px-10'>
-      <Balance>
+    <div className='mt-20 grid grid-cols-12 gap-4 px-10'>
+      <Balance className='col-span-4'>
         <Balance.Header>Balance</Balance.Header>
         <Balance.Amount amount={1234} />
       </Balance>
+      <Wallets className='col-span-8'>
+        <Wallets.Wallet>
+          <Wallets.Wallet.Header iconColor='#7C3AED'>
+            Cash
+          </Wallets.Wallet.Header>
+          <Wallets.Wallet.Amount amount={1234} />
+        </Wallets.Wallet>
+        <Wallets.Wallet>
+          <Wallets.Wallet.Header iconColor='#7C3AED'>
+            GCash
+          </Wallets.Wallet.Header>
+          <Wallets.Wallet.Amount amount={1234} />
+        </Wallets.Wallet>
+        <Wallets.Wallet>
+          <Wallets.Wallet.Header iconColor='#7C3AED'>
+            Paypal
+          </Wallets.Wallet.Header>
+          <Wallets.Wallet.Amount amount={1234} />
+        </Wallets.Wallet>
+        <Wallets.Wallet>
+          <Wallets.Wallet.Header iconColor='#7C3AED'>
+            Savings
+          </Wallets.Wallet.Header>
+          <Wallets.Wallet.Amount amount={1234} />
+        </Wallets.Wallet>
+      </Wallets>
     </div>
   );
 }
---
+++ src/components/sections/demo/wallets.tsx
@@ -0,0 +1,89 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+const Wallets = ({
+  className,
+  children
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <section
+      className={cn('flex flex-col gap-2 rounded-lg border p-8', className)}
+    >
+      <div className=''>
+        <h3 className='fontbold text-2xl'>Wallets</h3>
+      </div>
+      <div className='flex flex-row justify-between'>{children}</div>
+    </section>
+  );
+};
+
+const Wallet = ({
+  className,
+  children
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return <div className={cn('', className)}>{children}</div>;
+};
+
+const Header = ({
+  className,
+  children,
+  iconColor
+}: {
+  className?: string;
+  children?: React.ReactNode;
+  iconColor?: string;
+}) => {
+  return (
+    <div
+      className={cn('flex flex-row items-center gap-1 text-base', className)}
+    >
+      {iconColor ? (
+        <span
+          className='aspect-square h-[12px] rounded-full'
+          style={{ backgroundColor: iconColor }}
+        ></span>
+      ) : (
+        ''
+      )}
+      <h3>{children}</h3>
+    </div>
+  );
+};
+
+const Amount = ({
+  className,
+  amount
+}: {
+  className?: string;
+  amount: number;
+}) => {
+  let formattedAmount;
+
+  if (amount) {
+    formattedAmount = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'PHP',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).format(amount);
+  }
+
+  return (
+    <div className={cn('text-xl font-bold', className)}>
+      <p>{formattedAmount}</p>
+    </div>
+  );
+};
+
+Wallet.Header = Header;
+Wallet.Amount = Amount;
+
+Wallets.Wallet = Wallet;
+
+export default Wallets;
