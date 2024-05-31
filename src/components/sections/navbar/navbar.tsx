4c671c8ff7aaea400ec84ae6d27b525120d23385
unknown <michaelxaviercanonizado@gmail.com>
2024-06-01 02:02:01 +0800

    feat: contain navbar links using @/helpers/screen-breakpoints.ts

--- src/components/sections/navbar/navbar.tsx
+++ src/components/sections/navbar/navbar.tsx
@@ -1,10 +1,17 @@
+import { screenBreakpoints } from '@/helpers/screen-breakpoints';
+import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <header className='sticky top-0 w-full'>
-      <nav className='flex justify-between border border-b-gray-900 px-10 py-6'>
+    <header className='sticky top-0 w-full border border-b-gray-900'>
+      <nav
+        className={cn(
+          'mx-auto flex w-full justify-between  px-10 py-6',
+          screenBreakpoints
+        )}
+      >
         <div>
           <Link href='/'>
             <h1 className='text-2xl font-bold tracking-wide'>Minder</h1>
