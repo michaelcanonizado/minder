544056f545e08df9b67a26f6c3c481d69bf61ffa
unknown <michaelxaviercanonizado@gmail.com>
2024-05-31 21:19:05 +0800

    chore: add utility file

---
+++ src/app/demo/utilities.ts
@@ -0,0 +1,14 @@
+export const Tabs = [
+  {
+    title: 'Expense',
+    value: 'expense'
+  },
+  {
+    title: 'Income',
+    value: 'income'
+  },
+  {
+    title: 'Transfer',
+    value: 'transfer'
+  }
+];
