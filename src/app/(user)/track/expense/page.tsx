d5c7a2a9ddc844393b06efab9578a348e78bf7da
unknown <michaelxaviercanonizado@gmail.com>
2024-06-02 19:51:37 +0800

    chore: delete routes

--- src/app/(user)/track/expense/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TrackExpense = () => {
-  return <div>Track Expense</div>;
-};
-
-export default TrackExpense;
--- src/app/(user)/track/income/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TrackIncome = () => {
-  return <div>Track Income</div>;
-};
-
-export default TrackIncome;
