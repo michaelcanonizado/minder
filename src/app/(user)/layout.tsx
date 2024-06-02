4444b626a392f5eff61ebe336c26bddfeb214150
unknown <michaelxaviercanonizado@gmail.com>
2024-06-02 23:19:59 +0800

    chore: initialize Sidebar component

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,8 +1,10 @@
+import Sidebar from '@/components/sections/navigation/sidebar';
 import React from 'react';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
+      <Sidebar />
       <main className=''>{children}</main>
     </div>
   );
