f49c257842677f4a5635311cbf21416bba4e96d9
unknown <michaelxaviercanonizado@gmail.com>
2024-06-02 20:05:51 +0800

    feat: add links to all routes at /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,67 @@
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
+const links = [
+  {
+    name: 'Demo',
+    path: '/demo'
+  },
+  {
+    name: 'Dashboard',
+    path: '/dashboard'
+  },
+  {
+    name: 'Expense Tracker',
+    path: '/expense/track'
+  },
+  {
+    name: 'Expense Breakdown',
+    path: '/expense/breakdown'
+  },
+  {
+    name: 'Income Tracker',
+    path: '/income/track'
+  },
+  {
+    name: 'Income Breakdown',
+    path: '/income/breakdown'
+  },
+  {
+    name: 'Wallet Transfer',
+    path: '/wallet/transfer'
+  },
+  {
+    name: 'Wallet Breakdown',
+    path: '/wallet/breakdown'
+  }
+];
+
 export default function Home() {
   return (
-    <div className='mt-20 flex justify-center px-10'>
-      <Button variant='outline' className='w-full max-w-[250px]'>
-        <Link href='/demo'>Go to Demo</Link>
-      </Button>
+    <div className='mt-20 flex flex-col items-center'>
+      <div className='text-center'>
+        <div className=''>
+          <h1 className='text-4xl'>Links</h1>
+        </div>
+        <div className='mt-2 max-w-[300px]'>
+          <p className='text- text-sm'>
+            Temporary page to quickly access pages. Landing page will be built
+            here soon
+          </p>
+        </div>
+      </div>
+      <section className='mt-20 flex flex-col items-center justify-center gap-4 px-10 '>
+        {links.map(link => {
+          return (
+            <Button
+              variant='outline'
+              className='w-full max-w-[250px] first:mb-10'
+            >
+              <Link href={link.path}>{link.name}</Link>
+            </Button>
+          );
+        })}
+      </section>
     </div>
   );
 }
