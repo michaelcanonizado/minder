3e9b038275f37b1661d0f283d86053da8009701b
unknown <michaelxaviercanonizado@gmail.com>
2024-06-03 01:18:37 +0800

    chore: rename from navbar.tsx to guest-navbar.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Navbar from '@/components/sections/navigation/navbar';
+import Navbar from '@/components/sections/navigation/guest-navbar';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 
