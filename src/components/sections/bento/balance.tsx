ac1d41885c35781b986a1e7a8821dde9affb1365
unknown <michaelxaviercanonizado@gmail.com>
2024-06-04 00:28:23 +0800

    feat: add Bento.Balance content

--- src/components/sections/bento/balance.tsx
+++ src/components/sections/bento/balance.tsx
@@ -2,7 +2,55 @@ import { ArrowDown, ArrowUp } from 'lucide-react';
 import React from 'react';
 
 const Balance = ({ variant }: { variant: 'total' | 'expense' | 'income' }) => {
-  return <div className=''>Balance</div>;
+  const title = 'Total Balance';
+  const amount = '$25,808.15';
+  const percentageChange: {
+    isPositive: boolean;
+    timePeriod: 'weekly' | 'montly';
+    percentage: number;
+    difference: number;
+  } = {
+    isPositive: true,
+    timePeriod: 'weekly',
+    percentage: 0.9,
+    difference: 1539
+  };
+
+  const arrowIconClasses = 'w-[16px] h-fit';
+
+  const BalancePercentageChange = () => {
+    return (
+      <p className='text-body-200 flex flex-row text-muted-foreground'>
+        <span
+          className={`flex flex-row ${percentageChange.isPositive ? 'text-accent-100' : 'text-accent-200'}`}
+        >
+          {percentageChange.isPositive ? '+' : '-'}
+          {percentageChange.difference}(
+          {percentageChange.isPositive ? (
+            <ArrowUp className={arrowIconClasses} />
+          ) : (
+            <ArrowDown className={arrowIconClasses} />
+          )}
+          {percentageChange.percentage}%)
+        </span>
+        &nbsp;vs last month
+      </p>
+    );
+  };
+
+  return (
+    <div className=''>
+      <div className=''>
+        <p className='text-body-100 text-muted-foreground'>{title}</p>
+      </div>
+      <div className=''>
+        <h3 className='text-heading-100'>{amount}</h3>
+      </div>
+      <div className=''>
+        <BalancePercentageChange />
+      </div>
+    </div>
+  );
 };
 
 export default Balance;
