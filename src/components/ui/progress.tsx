54c4d94dd95c470f5a0a1da3e21ddb61a9fe2e3a
unknown <michaelxaviercanonizado@gmail.com>
2024-06-04 12:34:45 +0800

    chore: add shadcn progess

---
+++ src/components/ui/progress.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import * as React from 'react';
+import * as ProgressPrimitive from '@radix-ui/react-progress';
+
+import { cn } from '@/lib/utils';
+
+interface CustomProgressProp
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  fillColor?: string;
+}
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  CustomProgressProp
+>(({ className, fillColor, value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      'relative h-4 w-full overflow-hidden rounded-full bg-secondary',
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className='h-full w-full flex-1 rounded-full bg-primary transition-all'
+      style={{
+        transform: `translateX(-${100 - (value || 0)}%)`,
+        backgroundColor: fillColor
+      }}
+    />
+  </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
