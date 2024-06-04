2499811698b12465c0d54d95ee432fba3b627599
unknown <michaelxaviercanonizado@gmail.com>
2024-06-04 20:08:35 +0800

    chore: initialize <Form/> component

--- src/app/(user)/expense/track/page.tsx
+++ src/app/(user)/expense/track/page.tsx
@@ -1,7 +1,17 @@
+import Balance from '@/components/sections/balance';
+import Bento from '@/components/sections/bento';
 import React from 'react';
 
 const ExpenseTrack = () => {
-  return <div className='px-8'>Track Expense</div>;
+  return (
+    <div className='px-8'>
+      <div className=''></div>
+      <Bento>
+        <Bento.Box></Bento.Box>
+        <Bento.Box></Bento.Box>
+      </Bento>
+    </div>
+  );
 };
 
 export default ExpenseTrack;
