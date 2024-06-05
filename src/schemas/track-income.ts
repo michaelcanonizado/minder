a954d4adc88ef259e3ac45ac73fbdce7627f3dae
unknown <michaelxaviercanonizado@gmail.com>
2024-06-05 22:40:09 +0800

    refactor: add date property to schema

--- src/schemas/track-income.ts
+++ src/schemas/track-income.ts
@@ -4,13 +4,18 @@ const trackIncomeSchema = z.object({
   amount: z.coerce.number({ message: 'Please enter a valid amount' }).min(1, {
     message: 'Please enter the amount'
   }),
+  date: z.date({
+    message: 'Please enter the date'
+  }),
   walletId: z
     .string({ message: 'Please choose where to add' })
     .min(1, { message: 'Please choose where to add' }),
   categoryId: z
     .string({ message: 'Please choose the income category' })
     .min(1, { message: 'Please choose the income category' }),
-  description: z.string({ message: 'Please describe the expense' }).optional()
+  description: z
+    .string({ message: 'Please describe the income' })
+    .min(1, { message: 'Please describe the income' })
 });
 
 export default trackIncomeSchema;
