93f20256cf99fa1f085d188dd14808768fc91070
unknown <michaelxaviercanonizado@gmail.com>
2024-06-05 22:41:32 +0800

    chore: add wallets and categories data

--- src/components/sections/form/transaction/Income.tsx
+++ src/components/sections/form/transaction/Income.tsx
@@ -1,5 +1,118 @@
 import React from 'react';
 
+const wallets = [
+  {
+    id: '1',
+    name: 'Cash',
+    color: '#7C3AED',
+    description: '',
+    balance: 2810,
+    createdAt: new Date(),
+    transactionCount: 3,
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    }
+  },
+  {
+    id: '2',
+    name: 'GCash',
+    color: '#2994FC',
+    description: '',
+    balance: 500,
+    createdAt: new Date(),
+    transactionCount: 0,
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    }
+  },
+  {
+    id: '3',
+    name: 'Savings',
+    color: '#21C5E0',
+    description: '',
+    balance: 2000,
+    createdAt: new Date(),
+    transactionCount: 0,
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    }
+  },
+  {
+    id: '4',
+    name: 'Paypal',
+    color: '#F23E94',
+    description: '',
+    balance: 700,
+    createdAt: new Date(),
+    transactionCount: 0,
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    }
+  }
+];
+
+const categories = [
+  {
+    id: '1',
+    name: 'Salary',
+    createdAt: new Date(),
+    icon: '',
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    },
+    color: ''
+  },
+  {
+    id: '2',
+    name: 'Allowance',
+    createdAt: new Date(),
+    icon: '',
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    },
+    color: ''
+  },
+  {
+    id: '3',
+    name: 'Investments',
+    createdAt: new Date(),
+    icon: '',
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    },
+    color: ''
+  },
+  {
+    id: '4',
+    name: 'Business',
+    createdAt: new Date(),
+    icon: '',
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    },
+    color: ''
+  },
+  {
+    id: '5',
+    name: 'Other',
+    createdAt: new Date(),
+    icon: '',
+    isDeleted: {
+      status: false,
+      deletedAt: new Date()
+    },
+    color: ''
+  }
+];
+
 const Income = () => {
   return <div>Income</div>;
 };
