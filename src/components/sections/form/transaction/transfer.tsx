c7a103c2e3c13b4ad93d54b18980033a1ece32ae
unknown <michaelxaviercanonizado@gmail.com>
2024-06-05 23:16:28 +0800

    feat: copy code from <Form.Transaction.Expense/> and replace nessessary values

--- src/components/sections/form/transaction/transfer.tsx
+++ src/components/sections/form/transaction/transfer.tsx
@@ -1,4 +1,30 @@
+'use client';
+
 import React from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { cn } from '@/lib/utils';
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormControl,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+
+import {
+  FormDatePicker,
+  FormSelect,
+  FormRadioCardGroup,
+  FormInput
+} from '../components';
+
+import trackTransferSchema from '@/schemas/track-transfer';
 
 const wallets = [
   {
@@ -56,7 +82,118 @@ const wallets = [
 ];
 
 const Transfer = () => {
-  return <div>Transfer</div>;
+  const form = useForm<z.infer<typeof trackTransferSchema>>({
+    resolver: zodResolver(trackTransferSchema),
+    defaultValues: {
+      // I need to set each value a default value to remove the 'Warning: A component is changing an uncontrolled input to be controlled.' error from the <Input/> components. Amount is of type number, but i dont want to set its default value to 0 as i only want the placeholder to show, not prefill the <Input/> component. If you know a solution to this, feel free to pull request or commentat the repo.
+      // @ts-ignore
+      amount: '',
+      walletId: wallets[0].id,
+      categoryId: '',
+      date: undefined,
+      description: ''
+    }
+  });
+
+  const onSubmit = (data: z.infer<typeof trackTransferSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='mt-4 flex flex-col gap-4'
+      >
+        <div className='flex flex-col gap-1'>
+          <FormField
+            control={form.control}
+            name='sourceWalletId'
+            render={({ field }) => (
+              <FormItem className=''>
+                <FormLabel>From:</FormLabel>
+                <FormControl>
+                  <FormRadioCardGroup
+                    data={wallets}
+                    orientation='horizontal'
+                    field={field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='destinationWalletId'
+            render={({ field }) => (
+              <FormItem className=''>
+                <FormLabel>To:</FormLabel>
+                <FormControl>
+                  <FormRadioCardGroup
+                    data={wallets}
+                    orientation='horizontal'
+                    field={field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className='grid grid-cols-2 gap-4'>
+            <FormField
+              control={form.control}
+              name='amount'
+              render={({ field }) => (
+                <FormItem className=''>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <FormInput type='text' placeholder='125' {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='date'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Date</FormLabel>
+                  <FormDatePicker field={field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name='description'
+            render={({ field }) => (
+              <FormItem className=''>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <FormInput
+                    type='text'
+                    placeholder='Monthly salary'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type='submit' className='w-full'>
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Transfer;
