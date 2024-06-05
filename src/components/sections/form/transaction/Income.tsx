d3207796835f69b7719c5a0c715a8f0234cd1fd2
unknown <michaelxaviercanonizado@gmail.com>
2024-06-05 22:46:19 +0800

    feat: copy code from <Form.Transaction.Income/> and replace needed values

--- src/components/sections/form/transaction/Income.tsx
+++ src/components/sections/form/transaction/Income.tsx
@@ -1,4 +1,30 @@
+'use client';
+
 import React from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { cn } from '@/lib/utils';
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormControl,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+
+import {
+  FormDatePicker,
+  FormSelect,
+  FormRadioCardGroup,
+  FormInput
+} from '../components';
+
+import trackIncomeSchema from '@/schemas/track-expense';
 
 const wallets = [
   {
@@ -114,7 +140,112 @@ const categories = [
 ];
 
 const Income = () => {
-  return <div>Income</div>;
+  const form = useForm<z.infer<typeof trackIncomeSchema>>({
+    resolver: zodResolver(trackIncomeSchema),
+    defaultValues: {
+      // I need to set each value a default value to remove the 'Warning: A component is changing an uncontrolled input to be controlled.' error from the <Input/> components. Amount is of type number, but i dont want to set its default value to 0 as i only want the placeholder to show, not prefill the <Input/> component. If you know a solution to this, feel free to pull request or commentat the repo.
+      // @ts-ignore
+      amount: '',
+      walletId: categories[0].id,
+      categoryId: '',
+      date: undefined,
+      description: ''
+    }
+  });
+
+  const onSubmit = (data: z.infer<typeof trackIncomeSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='mt-4 flex flex-col gap-4'
+      >
+        <div className='flex flex-col gap-1'>
+          <FormField
+            control={form.control}
+            name='walletId'
+            render={({ field }) => (
+              <FormItem className=''>
+                <FormLabel>Add to:</FormLabel>
+                <FormControl>
+                  <FormRadioCardGroup
+                    data={wallets}
+                    orientation='horizontal'
+                    field={field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name='amount'
+            render={({ field }) => (
+              <FormItem className=''>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <FormInput type='text' placeholder='125' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className='grid grid-cols-2 gap-4'>
+            <FormField
+              control={form.control}
+              name='categoryId'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <FormSelect data={categories} field={field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name='date'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Date</FormLabel>
+                  <FormDatePicker field={field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <FormField
+            control={form.control}
+            name='description'
+            render={({ field }) => (
+              <FormItem className=''>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <FormInput
+                    type='text'
+                    placeholder='Monthly salary'
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type='submit' className='w-full'>
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Income;
