43c06cb3a7ac15b6d5c996468824effb1a0c4200
unknown <michaelxaviercanonizado@gmail.com>
2024-06-05 22:59:57 +0800

    feat: add trackTransferSchema

---
+++ src/schemas/track-transfer.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+const trackTransferSchema = z.object({
+  amount: z.coerce.number({ message: 'Please enter a valid amount' }).min(1, {
+    message: 'Please enter the amount'
+  }),
+  date: z.date({
+    message: 'Please enter the date'
+  }),
+  sourceWalletId: z
+    .string({ message: 'Please choose the source' })
+    .min(1, { message: 'Please choose the source' }),
+  destinationWalletId: z
+    .string({ message: 'Please choose the destination' })
+    .min(1, { message: 'Please choose the destination' }),
+  description: z
+    .string({ message: 'Please describe the income' })
+    .min(1, { message: 'Please describe the income' })
+});
+
+export default trackTransferSchema;
