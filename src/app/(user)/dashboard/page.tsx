6c0b74a53fd9fec764fe2f2f8076dda7f90a5d8c
unknown <michaelxaviercanonizado@gmail.com>
2024-06-07 00:15:44 +0800

    feat: make balance charts into a tab group

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,38 +1,53 @@
 import React from 'react';
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 import Balance from '@/components/sections/balance';
 import Bento from '@/components/sections/bento';
 import Chart from '@/components/sections/chart';
 
 const Dashboard = () => {
+  const balances = [
+    {
+      name: 'Total'
+    },
+    {
+      name: 'Income'
+    },
+    {
+      name: 'Expense'
+    }
+  ];
+
   return (
     <div className='px-8'>
-      <Bento className='grid-cols-3'>
+      <Bento className='grid-cols-1'>
         <Bento.Box>
-          <Bento.Box.Header>
-            <Balance.Compact variant='total' />
-          </Bento.Box.Header>
-          <Bento.Box.Content>
-            <Chart.Line.Minimal />
-          </Bento.Box.Content>
+          <Tabs defaultValue='Total' className='w-full'>
+            <TabsList className='w-full rounded-b-none'>
+              {balances.map(item => {
+                return (
+                  <TabsTrigger value={item.name} className='grow'>
+                    {item.name}
+                  </TabsTrigger>
+                );
+              })}
+            </TabsList>
+            {balances.map(item => {
+              return (
+                <TabsContent value={item.name}>
+                  <Bento.Box.Header>
+                    <Balance.Compact variant='total' />
+                  </Bento.Box.Header>
+                  <Bento.Box.Content>
+                    <Chart.Line.Minimal />
+                  </Bento.Box.Content>
+                </TabsContent>
+              );
+            })}
+          </Tabs>
         </Bento.Box>
-        <Bento.Box>
-          <Bento.Box.Header>
-            <Balance.Compact variant='total' />
-          </Bento.Box.Header>
-          <Bento.Box.Content>
-            <Chart.Line.Minimal />
-          </Bento.Box.Content>
-        </Bento.Box>
-        <Bento.Box>
-          <Bento.Box.Header>
-            <Balance.Compact variant='total' />
-          </Bento.Box.Header>
-          <Bento.Box.Content>
-            <Chart.Line.Minimal />
-          </Bento.Box.Content>
-        </Bento.Box>
-        <Bento.Box className='col-span-3'>
+        <Bento.Box className=''>
           <Bento.Box.Header>
             <Balance.Compact variant='weekly' />
           </Bento.Box.Header>
