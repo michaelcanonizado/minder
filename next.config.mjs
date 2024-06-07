c10b0d3d5fe620dfa2d9c5a236eb7380b5c4ef6b
unknown <michaelxaviercanonizado@gmail.com>
2024-06-07 19:20:38 +0800

    fix: comment out mongoose requirements as pages were not compiling

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Mongoose requirements as per: https://mongoosejs.com/docs/nextjs.html
-  experimental: {
-    esmExternals: 'loose',
-    serverComponentsExternalPackages: ['mongoose']
-  },
-  webpack: config => {
-    config.experiments = {
-      topLevelAwait: true
-    };
-    return config;
-  }
-};
+const nextConfig = {};
+// const nextConfig = {
+//   // Mongoose requirements as per: https://mongoosejs.com/docs/nextjs.html
+//   experimental: {
+//     esmExternals: 'loose',
+//     serverComponentsExternalPackages: ['mongoose']
+//   },
+//   webpack: config => {
+//     config.experiments = {
+//       topLevelAwait: true
+//     };
+//     return config;
+//   }
+// };
 
 export default nextConfig;
