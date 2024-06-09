3935df8da3dbbb9e3a93ced7ddc912dfaf916318
unknown <michaelxaviercanonizado@gmail.com>
2024-06-10 00:31:45 +0800

    feat: get incomes data

--- src/app/(user)/income/breakdown/page.tsx
+++ src/app/(user)/income/breakdown/page.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const IncomeBreakdown = () => {
+import { getIncomesData } from '@/lib/get-incomes-data';
+
+const IncomeBreakdown = async () => {
+  const page: number = 1;
+  const limit: number = 4;
+  const data = await getIncomesData({ page, limit });
+
+  console.log(data);
+  console.log(`Data count: ${data.length}`);
+
   return <div className='px-8'>Income Breakdown</div>;
 };
 
