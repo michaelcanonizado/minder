22407e33cde15ff0840c8ae540e6a6ff1e4c735e
unknown <michaelxaviercanonizado@gmail.com>
2024-06-09 22:45:11 +0800

    fix: update Model properties and reseed income

--- src/seeds/income.ts
+++ src/seeds/income.ts
@@ -39,9 +39,9 @@ const seedIncome = async () => {
     totalAmount += randAmount;
 
     incomeLogs.push({
-      userId: user._id,
-      walletId: user.wallets[i]._id,
-      categoryId: user.categories?.income[randCategory]._id,
+      user: user._id,
+      wallet: user.wallets[i]._id,
+      category: user.categories?.income[randCategory]._id,
       amount: randAmount,
       description: descriptions[randDescription],
       transactionDate: new Date()
