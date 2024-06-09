e7d89411169224521995c394b14c50e9950fcf99
unknown <michaelxaviercanonizado@gmail.com>
2024-06-10 00:16:13 +0800

    feat: add Expense model type

--- src/models/expense.ts
+++ src/models/expense.ts
@@ -1,4 +1,5 @@
 import mongoose, { InferSchemaType } from 'mongoose';
+import { UserCategoryType, UserType, UserWalletType } from './user';
 
 const expenseSchema = new mongoose.Schema(
   {
@@ -30,6 +31,18 @@ const expenseSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+export interface ExpenseType {
+  user: UserType;
+  wallet: UserWalletType;
+  category: UserCategoryType;
+  amount: number;
+  description: number;
+  transactionDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  __v?: number;
+}
 
 const Expense =
   mongoose.models.Expense ||
