b5a577723df9fffb143742d2c7f4b11cc5de85bd
unknown <michaelxaviercanonizado@gmail.com>
2024-06-10 21:40:54 +0800

    feat: add expenses data-table

--- src/app/(user)/expense/breakdown/page.tsx
+++ src/app/(user)/expense/breakdown/page.tsx
@@ -1,15 +1,34 @@
-import { getExpensesData } from '@/lib/get-expenses-data';
 import React from 'react';
+
+import { getExpensesData } from '@/lib/get-expenses-data';
+import Bento from '@/components/sections/bento';
+import Table from '@/components/sections/table';
+import { columns } from './columns';
 
 const ExpenseBreakdown = async () => {
   const page: number = 3;
-  const limit: number = 2;
+  const limit: number = 10;
   const data = await getExpensesData({ page, limit });
 
   console.log(data);
   console.log(`Data count: ${data.length}`);
 
-  return <div className='px-8'>Expense Breakdown</div>;
+  return (
+    <div className='px-8'>
+      <div className=''>
+        <Bento className='grid-cols-1'>
+          <Bento.Box>
+            <Bento.Box.Header className='text-display'>
+              Expenses Breakdown
+            </Bento.Box.Header>
+            <Bento.Box.Content className='p-0'>
+              <Table.DataTable.Scroll columns={columns} data={data} />
+            </Bento.Box.Content>
+          </Bento.Box>
+        </Bento>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseBreakdown;
