0420bd719c6e8ff16269ce39ebda6ff7d92e1ce6
unknown <michaelxaviercanonizado@gmail.com>
2024-06-11 19:31:40 +0800

    refactor: use try and catch to connect and close the database

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -1,19 +1,25 @@
 import mongoose from 'mongoose';
 
 export const databaseConnect = async () => {
-  return mongoose
-    .connect('mongodb://localhost:27017/minder-finance')
-    .then(() => {
-      console.log('Mongo connected!');
-    })
-    .catch(error => {
-      console.log('Mongo failed to connect!');
-      console.log(error);
-    });
+  try {
+    await mongoose.connect('mongodb://localhost:27017/minder-finance');
+    console.log('Database connected!');
+  } catch (err) {
+    console.log('Database failed to connect!');
+    console.log(err);
+  }
+
+  return;
 };
 
 export const databaseClose = async () => {
-  await mongoose.connection.close();
-  console.log('Mongo connection closed!');
+  try {
+    await mongoose.connection.close();
+    console.log('Database connection closed!');
+  } catch (err) {
+    console.log('Error closing connection to the database!');
+    console.log(err);
+  }
+
   return;
 };
