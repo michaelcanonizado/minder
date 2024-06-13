7439a58086fae50b081bec00326b8993e6c956fe
unknown <michaelxaviercanonizado@gmail.com>
2024-06-13 23:09:10 +0800

    feat: add balance transfer data table

--- src/app/(user)/wallet/breakdown/page.tsx
+++ src/app/(user)/wallet/breakdown/page.tsx
@@ -1,5 +1,11 @@
-import { getBalanceTransfersData } from '@/lib/get-balance-transfers-data';
 import React from 'react';
+
+import Bento from '@/components/sections/bento';
+import Table from '@/components/sections/table';
+import { columns } from './columns';
+import Pagination from '@/components/sections/pagination';
+
+import { getBalanceTransfersData } from '@/lib/get-balance-transfers-data';
 
 const WalletBreakdown = async ({
   searchParams
@@ -9,13 +15,36 @@ const WalletBreakdown = async ({
   let page =
     typeof searchParams.page == 'string' ? Number(searchParams.page) : 1;
   page = page < 1 ? 1 : page;
-  const limit = 1;
+  const limit = 10;
 
   const balanceTransfers = await getBalanceTransfersData({ page, limit });
 
   console.log(balanceTransfers);
 
-  return <div className='px-8'>Wallet Breakdown</div>;
+  return (
+    <div className='px-8'>
+      <div className=''>
+        <Bento className='grid-cols-1'>
+          <Bento.Box>
+            <Bento.Box.Header className='text-display'>
+              Balance Transfers
+            </Bento.Box.Header>
+            <Bento.Box.Content className='space-y-4 p-0 pb-4'>
+              <Table.DataTable.Scroll
+                columns={columns}
+                data={balanceTransfers.data}
+              />
+              <Pagination
+                pathname='/wallet/breakdown'
+                currentPage={page}
+                pagesCount={balanceTransfers.pages.max}
+              />
+            </Bento.Box.Content>
+          </Bento.Box>
+        </Bento>
+      </div>
+    </div>
+  );
 };
 
 export default WalletBreakdown;
