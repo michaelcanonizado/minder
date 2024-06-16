31519bdc22afcf1468f549a3de42ffe3b8c9ad5f
unknown <michaelxaviercanonizado@gmail.com>
2024-06-16 22:47:52 +0800

    chore: delete deprecated code

--- src/seeds/snapshot.ts
+++ src/seeds/snapshot.ts
@@ -2,26 +2,11 @@ import { databaseConnect, databaseClose } from '@/helpers/database';
 import User from '@/models/user';
 import Expense from '@/models/expense';
 import Income from '@/models/income';
-
-const getLastWeekSunday = () => {
-  const today = new Date();
-
-  // 0 for Sunday, 1 for Monday, ..., 6 for Saturday
-  const dayOfWeek = today.getDay();
-
-  // Make a copy of today's date
-  const prevSunday = new Date(today);
-
-  // Calculate how many days to subtract to get to the previous Sunday
-  prevSunday.setDate(prevSunday.getDate() - dayOfWeek);
-
-  // Set time to 11:59:59 PM
-  prevSunday.setHours(23, 59, 59, 999);
-
-  return prevSunday;
-};
+import { getLastWeekDateLimit } from '@/helpers/get-last-week-date-limit';
 
 const seedSnapshot = async () => {
+  // DEPRECATED CODE!!!!
+
   await databaseConnect();
 
   // Get user
@@ -32,7 +17,7 @@ const seedSnapshot = async () => {
   }
   console.log('User found!');
 
-  const lastSundayDate = getLastWeekSunday();
+  const lastSundayDate = getLastWeekDateLimit();
 
   const pipeline = [
     {
@@ -63,24 +48,28 @@ const seedSnapshot = async () => {
   const totalExpense = expenseRes[0] ? expenseRes[0].totalAmount : 0;
   const totalIncome = incomeRes[0] ? incomeRes[0].totalAmount : 0;
 
-  // @ts-ignore
-  user.snapshots.push({
-    balance: {
-      totalBalance: totalIncome - totalExpense,
-      totalIncome: totalIncome,
-      totalExpense: totalExpense
-    },
-    snapshotDateLimit: lastSundayDate
-  });
+  // DEPRECATED CODE!!!!
 
-  console.log(user.snapshots);
-  console.log(totalExpense);
-  console.log(totalIncome);
+  // @ts-ignore
+  // user.snapshots.push({
+  //   balance: {
+  //     totalBalance: totalIncome - totalExpense,
+  //     totalIncome: totalIncome,
+  //     totalExpense: totalExpense
+  //   },
+  //   snapshotDateLimit: lastSundayDate
+  // });
+
+  // console.log(user.snapshots);
+  // console.log(totalExpense);
+  // console.log(totalIncome);
 
   await user.save();
 
   await databaseClose();
   return;
+
+  // DEPRECATED CODE!!!!
 };
 
 seedSnapshot();
