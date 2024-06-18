8eef15473823937ff065302e4e26261088efbb0e
unknown <michaelxaviercanonizado@gmail.com>
2024-06-18 18:50:34 +0800

    faet: add code to seed incomes coming from google sheets

--- src/seeds/from-google-sheets.ts
+++ src/seeds/from-google-sheets.ts
@@ -1,27 +1,79 @@
 import { authenticateGoogleAPI } from '@/helpers/authenticate-google-api';
+import { databaseClose, databaseConnect } from '@/helpers/database';
+import Income from '@/models/income';
+import User from '@/models/user';
 
 export const seedFromGoogleSheets = async () => {
-  const spreadsheetId = process.env.GOOGLE_SHEET_ID;
-
   const [googleSheets, auth] = await authenticateGoogleAPI();
 
-  const rows = await googleSheets.spreadsheets.values.get({
+  await databaseConnect();
+
+  const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+  const userId = process.env.TEMP_USER_ID;
+
+  const user = await User.findById(userId);
+  if (!user) {
+    console.log('User not found!');
+    return;
+  }
+
+  const incomeRows = await googleSheets.spreadsheets.values.get({
     auth,
     spreadsheetId,
     range: 'INCOME DATABASE'
   });
+  const incomeSheetRows = incomeRows.data.values ? incomeRows.data.values : [];
 
-  console.log(rows.data.values);
+  await Income.deleteMany({});
+  for (let i = 1; i < incomeSheetRows.length; i++) {
+    const wallet = user.wallets.find(wallet => {
+      if (
+        wallet.name.toLowerCase().includes(incomeSheetRows[i][5].toLowerCase())
+      ) {
+        return wallet;
+      }
+    });
 
-  //   {
-  //     user: user._id,
-  //     wallet: user.wallets[i]._id,
-  //     category: user.categories?.income[randCategory]._id,
-  //     amount: randAmount,
-  //     description: descriptions[randDescription],
-  //     transactionDate: new Date()
-  //   }
+    const category = user.categories.income.find(category => {
+      if (
+        category.name
+          .toLowerCase()
+          .includes(incomeSheetRows[i][3].toLowerCase())
+      ) {
+        return category;
+      }
+    });
 
-  const formattedDate = {};
+    const amount: number = parseFloat(incomeSheetRows[i][2]);
+    const description: string = incomeSheetRows[i][4]
+      ? incomeSheetRows[i][4]
+      : '-';
+
+    const res = new Income({
+      user: userId,
+      wallet: wallet ? wallet._id : null,
+      category: category ? category._id : null,
+      amount: amount,
+      description: description,
+      transactionDate: new Date(incomeSheetRows[i][1]),
+      createdAt: new Date(incomeSheetRows[i][0]),
+      updatedAt: new Date(incomeSheetRows[i][0])
+    });
+
+    if (wallet) {
+      wallet.balance += amount;
+    }
+
+    user.balance.netBalance += amount;
+    user.balance.totalIncome += amount;
+
+    await res.save();
+    console.log(res);
+  }
+
+  await user.save();
+
+  await databaseClose();
+  return;
 };
 seedFromGoogleSheets();
