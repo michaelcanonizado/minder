072c9027cedaab03ee85b39c8b747cbeac258834
unknown <michaelxaviercanonizado@gmail.com>
2024-06-18 19:39:38 +0800

    faet: add code to seed expenses coming from google sheets

--- src/seeds/from-google-sheets.ts
+++ src/seeds/from-google-sheets.ts
@@ -1,5 +1,6 @@
 import { authenticateGoogleAPI } from '@/helpers/authenticate-google-api';
 import { databaseClose, databaseConnect } from '@/helpers/database';
+import Expense from '@/models/expense';
 import Income from '@/models/income';
 import User from '@/models/user';
 
@@ -76,11 +77,65 @@ export const seedFromGoogleSheets = async () => {
     user.balance.totalIncome += amount;
 
     await res.save();
-    console.log(res);
+  }
+
+  const expenseRows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: 'EXPENSES DATABASE'
+  });
+  const expenseSheetRows = expenseRows.data.values
+    ? expenseRows.data.values
+    : [];
+
+  await Expense.deleteMany({});
+  for (let i = 1; i < expenseSheetRows.length; i++) {
+    const wallet = user.wallets.find(wallet => {
+      if (
+        wallet.name.toLowerCase().includes(expenseSheetRows[i][5].toLowerCase())
+      ) {
+        return wallet;
+      }
+    });
+
+    const category = user.categories.expense.find(category => {
+      if (
+        category.name
+          .toLowerCase()
+          .includes(expenseSheetRows[i][3].toLowerCase())
+      ) {
+        return category;
+      }
+    });
+
+    const amount: number = parseFloat(expenseSheetRows[i][2]);
+    const description: string = expenseSheetRows[i][4]
+      ? expenseSheetRows[i][4]
+      : '-';
+
+    const res = new Expense({
+      user: userId,
+      wallet: wallet ? wallet._id : null,
+      category: category ? category._id : null,
+      amount: amount,
+      description: description,
+      transactionDate: new Date(expenseSheetRows[i][1]),
+      createdAt: new Date(expenseSheetRows[i][0]),
+      updatedAt: new Date(expenseSheetRows[i][0])
+    });
+
+    if (wallet) {
+      wallet.balance -= amount;
+    }
+    user.balance.netBalance -= amount;
+    user.balance.totalExpense += amount;
+
+    await res.save();
   }
 
   // Save changes to User document
   await user.save();
+  console.log(user);
 
   await databaseClose();
   return;
