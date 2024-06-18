380bccd371971347458cbe38e2920134a2f18a08
unknown <michaelxaviercanonizado@gmail.com>
2024-06-18 16:09:44 +0800

    feat: add authenticateGoogleApi()

--- src/helpers/authenticate-google-api.ts
+++ src/helpers/authenticate-google-api.ts
@@ -1,4 +1,25 @@
+import { google } from 'googleapis';
+
 export const authenticateGoogleAPI = async () => {
-  console.log('Authenticating Google API...');
-  return;
+  const keyFilePath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+  // Authenticate
+  const auth = new google.auth.GoogleAuth({
+    keyFile: keyFilePath,
+    scopes: 'https://www.googleapis.com/auth/spreadsheets'
+  });
+
+  // Create client instance
+  const client = await auth.getClient();
+
+  // Create instance of Google Sheets API
+  // @ts-ignore
+  const googleSheets = google.sheets({ version: 'v4', auth: client });
+
+  // Throw error if an error occurred when authenticating
+  if (!googleSheets) {
+    throw new Error('Error connecting with google API');
+  }
+
+  return [googleSheets, auth] as [typeof googleSheets, typeof auth];
 };
--- src/seeds/from-google-sheets.ts
+++ src/seeds/from-google-sheets.ts
@@ -1,7 +1,27 @@
 import { authenticateGoogleAPI } from '@/helpers/authenticate-google-api';
 
 export const seedFromGoogleSheets = async () => {
-  await authenticateGoogleAPI();
-  console.log('seeding....');
+  const spreadsheetId = process.env.GOOGLE_SHEET_ID;
+
+  const [googleSheets, auth] = await authenticateGoogleAPI();
+
+  const rows = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: 'INCOME DATABASE'
+  });
+
+  console.log(rows.data.values);
+
+  //   {
+  //     user: user._id,
+  //     wallet: user.wallets[i]._id,
+  //     category: user.categories?.income[randCategory]._id,
+  //     amount: randAmount,
+  //     description: descriptions[randDescription],
+  //     transactionDate: new Date()
+  //   }
+
+  const formattedDate = {};
 };
 seedFromGoogleSheets();
