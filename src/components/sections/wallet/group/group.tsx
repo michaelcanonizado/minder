b13826e672aa013fd084650a28bb2a7ef4867944
unknown <michaelxaviercanonizado@gmail.com>
2024-06-19 23:41:34 +0800

    fix: make wallet alignments match the rest of the sidebar links

--- src/components/sections/wallet/group/group.tsx
+++ src/components/sections/wallet/group/group.tsx
@@ -12,8 +12,8 @@ const Group = async ({ className }: { className?: string }) => {
     <ul className={cn('flex flex-col gap-4', className)}>
       {wallets.data.map(wallet => {
         return (
-          <div className='flex flex-row gap-4'>
-            <ChevronRight />
+          <div className='flex flex-row gap-2 rounded-lg py-2 pl-2'>
+            <ChevronRight className='min-w-[24px]' />
             <Balance.Slim>
               <Balance.Slim.Header>{wallet.name}</Balance.Slim.Header>
               <div className='flex flex-col items-end'>
