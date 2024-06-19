ff3025a010bb2fd68a8c20068d292adf84a521ee
unknown <michaelxaviercanonizado@gmail.com>
2024-06-20 00:13:23 +0800

    feat: add zod schema and userForm to <Form.Add.Wallet/>

--- src/components/sections/form/add/wallet.tsx
+++ src/components/sections/form/add/wallet.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import React from 'react';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import { cn } from '@/lib/utils';
 
+import { FormInput } from '../components';
+
+import { usePathname } from 'next/navigation';
+import addWalletSchema from '@/schemas/add-wallet';
+import { Form } from '@/components/ui/form';
+
 const Wallet = ({ className }: { className?: string }) => {
-  return <div className={cn('', className)}>Add Wallet</div>;
+  const userId = process.env.TEMP_USER_ID!;
+  const currentPathname = usePathname();
+
+  const form = useForm<z.infer<typeof addWalletSchema>>({
+    resolver: zodResolver(addWalletSchema),
+    defaultValues: {
+      name: '',
+      balance: 0,
+      userId: userId,
+      formPath: currentPathname
+    }
+  });
+
+  const onSubmit = async (data: z.infer<typeof addWalletSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form className={cn('', className)}></form>
+    </Form>
+  );
 };
 
 export default Wallet;
