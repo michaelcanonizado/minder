f20dddf5b31fe746146ed950776756abba5075c8
unknown <michaelxaviercanonizado@gmail.com>
2024-06-20 00:01:34 +0800

    feat: add zod addWalletSchema

---
+++ src/schemas/add-wallet.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+const addWalletSchema = z.object({
+  name: z
+    .string({ message: 'Please name the wallet' })
+    .min(1, { message: 'Please name the wallet' }),
+  balance: z.coerce
+    .number({ message: 'Please enter a valid amount' })
+    .optional(),
+  userId: z
+    .string({ message: 'Error! Please try again.' })
+    .min(1, { message: 'Error! Please try again.' }),
+  formPath: z
+    .string({ message: 'Error! Please try again.' })
+    .min(1, { message: 'Error! Please try again.' })
+});
+
+export default addWalletSchema;
