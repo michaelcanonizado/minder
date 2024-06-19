202353b3e710271c74ffee5fabd232a5277f54a2
unknown <michaelxaviercanonizado@gmail.com>
2024-06-19 22:08:25 +0800

    refactor: remove wallet group header and move it to the parent component

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -5,7 +5,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='max-w-screen flex w-full flex-row'>
       <Navbar.User.Vertical className='hidden min-w-[300px] md:flex'>
-        <Wallet.Group />
+        <div className='flex flex-col gap-2'>
+          <p className='text-muted-foreground'>Wallets</p>
+          <Wallet.Group />
+        </div>
       </Navbar.User.Vertical>
       <main className='w-full max-w-full overflow-x-hidden'>
         <Navbar.User.Horizontal />
