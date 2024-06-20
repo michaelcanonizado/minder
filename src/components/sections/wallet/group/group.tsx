6ece9513ad7200c935e0f88639af87a52d76c423
unknown <michaelxaviercanonizado@gmail.com>
2024-06-20 15:50:56 +0800

    fix: add key prop to each rendered wallet

--- src/components/sections/wallet/group/group.tsx
+++ src/components/sections/wallet/group/group.tsx
@@ -12,7 +12,10 @@ const Group = async ({ className }: { className?: string }) => {
     <ul className={cn('flex flex-col gap-4', className)}>
       {wallets.data.map(wallet => {
         return (
-          <div className='flex flex-row gap-2 rounded-lg py-2 pl-2'>
+          <div
+            className='flex flex-row gap-2 rounded-lg py-2 pl-2'
+            key={wallet._id}
+          >
             <ChevronRight className='min-w-[24px]' />
             <Balance.Slim>
               <Balance.Slim.Header>{wallet.name}</Balance.Slim.Header>
