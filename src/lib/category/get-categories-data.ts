9e38de47dc481f6066c7710ef5bfc653c207b8da
unknown <michaelxaviercanonizado@gmail.com>
2024-06-21 21:03:14 +0800

    feat: add getCategoriesData() server action

---
+++ src/lib/category/get-categories-data.ts
@@ -0,0 +1,20 @@
+import { databaseConnect } from '@/helpers/database';
+import User, { UserType } from '@/models/user';
+
+export interface WalletsDataType {
+  data: Pick<UserType, 'categories'>['categories'];
+}
+
+export const getCategoriesData = async (userId: string) => {
+  await databaseConnect();
+
+  const user = await User.findById(userId);
+
+  if (!user) {
+    throw new Error('User not found!');
+  }
+
+  return {
+    data: JSON.parse(JSON.stringify(user.categories))
+  };
+};
