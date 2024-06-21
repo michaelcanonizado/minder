27be269dcdc5fad1faa4bef81e107090b2896419
unknown <michaelxaviercanonizado@gmail.com>
2024-06-21 21:51:54 +0800

    feat: add component styles

--- src/components/sections/chart/label/label.tsx
+++ src/components/sections/chart/label/label.tsx
@@ -1,9 +1,16 @@
+import { cn } from '@/lib/utils';
 import React from 'react';
 
-const Label = ({ children }: { children: React.ReactNode }) => {
+const Label = ({
+  children,
+  className
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className=''>
-      <p className=''>{children}</p>
+    <div className='text-body-200 text-muted-foreground'>
+      <p className={cn('', className)}>{children}</p>
     </div>
   );
 };
