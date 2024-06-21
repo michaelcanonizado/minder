7837bbc9c91247408d548e7d8510e8fae031404d
unknown <michaelxaviercanonizado@gmail.com>
2024-06-21 21:56:48 +0800

    feat: add custom chart label: <Chart.Label/> to area chart

--- src/app/(user)/dashboard/monthly-expense.tsx
+++ src/app/(user)/dashboard/monthly-expense.tsx
@@ -1,5 +1,6 @@
 import Chart from '@/components/sections/chart';
 import { getExpensesGraphData } from '@/lib/expense/get-expenses-graph-data';
+import { format } from 'date-fns';
 
 const MonthlyExpense = async () => {
   const userId = process.env.TEMP_USER_ID!;
@@ -7,7 +8,17 @@ const MonthlyExpense = async () => {
   const data = await getExpensesGraphData(userId, 'monthly');
 
   return (
-    <Chart.Area data={data} index='transactionDate' categories={['amount']} />
+    <>
+      <Chart.Area data={data} index='transactionDate' categories={['amount']} />
+      <div className='flex flex-row justify-between px-4 pb-4 pt-8'>
+        <Chart.Label>
+          {format(data[0].transactionDate, 'E, MMM d, yyyy')}
+        </Chart.Label>
+        <Chart.Label>
+          {format(data[data.length - 1].transactionDate, 'E, MMM d, yyyy')}
+        </Chart.Label>
+      </div>
+    </>
   );
 };
 
