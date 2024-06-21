795ec28f85a04ea83ed8d1d6d4aeb46435f516e5
unknown <michaelxaviercanonizado@gmail.com>
2024-06-21 23:20:19 +0800

    feat: format date property before rendering area chart

--- src/app/(user)/dashboard/weekly-expense.tsx
+++ src/app/(user)/dashboard/weekly-expense.tsx
@@ -1,19 +1,21 @@
 import Chart from '@/components/sections/chart';
 import { getExpensesChartData } from '@/lib/expense/get-expenses-chart-data';
-import { format } from 'date-fns';
+import { formatChartDataDateProperties } from '@/helpers/format/format-chart-data-date-properties';
 
 const WeeklyExpense = async () => {
   const userId = process.env.TEMP_USER_ID!;
 
   const data = await getExpensesChartData(userId, 'weekly');
 
+  const formattedData = formatChartDataDateProperties(data);
+
   return (
     <>
-      <Chart.Area data={data} index='date' categories={['amount']} />
+      <Chart.Area data={formattedData} index='date' categories={['amount']} />
       <div className='flex flex-row justify-between px-4 pb-4 pt-8'>
-        <Chart.Label>{format(data[0].date, 'E, MMM d, yyyy')}</Chart.Label>
+        <Chart.Label>{formattedData[0].date}</Chart.Label>
         <Chart.Label>
-          {format(data[data.length - 1].date, 'E, MMM d, yyyy')}
+          {formattedData[formattedData.length - 1].date}
         </Chart.Label>
       </div>
     </>
