c60b505508c58360a84d7a81b1cb4f28164016fc
unknown <michaelxaviercanonizado@gmail.com>
2024-06-21 21:37:38 +0800

    feat: get monthly expenses graph data and use it to render the area chart

--- src/app/(user)/dashboard/monthly-expense.tsx
+++ src/app/(user)/dashboard/monthly-expense.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import Chart from '@/components/sections/chart';
+import { getExpensesGraphData } from '@/lib/expense/get-expenses-graph-data';
 
-const MonthlyExpense = () => {
-  return <div>MonthlyExpense</div>;
+const MonthlyExpense = async () => {
+  const userId = process.env.TEMP_USER_ID!;
+
+  const data = await getExpensesGraphData(userId, 'monthly');
+
+  console.log(data);
+
+  return (
+    <Chart.Area data={data} index='transactionDate' categories={['amount']} />
+  );
 };
 
 export default MonthlyExpense;
