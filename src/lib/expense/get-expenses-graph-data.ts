817fe3b3d869197455269f4fe9cf4219a2c4ad11
unknown <michaelxaviercanonizado@gmail.com>
2024-06-21 21:33:10 +0800

    feat: add getExpensesGraphData() server action

---
+++ src/lib/expense/get-expenses-graph-data.ts
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+import { databaseConnect } from '@/helpers/database';
+import Expense from '@/models/expense';
+
+import { getLastWeekStartAndEndDates } from '@/helpers/dates/get-last-week-start-and-end-dates';
+import { getThisWeekStartAndEndDates } from '@/helpers/dates/get-this-week-start-and-end-dates';
+import { getThisMonthStartAndEndDates } from '@/helpers/dates/get-this-month-start-and-end-dates';
+import { getLastMonthStartAndEndDates } from '@/helpers/dates/get-last-month-start-and-end-dates';
+
+export const getExpensesGraphData = async (
+  userId: string,
+  period: 'weekly' | 'monthly'
+) => {
+  await databaseConnect();
+
+  let startDate: Date | null = null;
+  let endDate: Date | null = null;
+
+  if (period === 'weekly') {
+    const thisWeek = getThisWeekStartAndEndDates();
+    const lastWeek = getLastWeekStartAndEndDates();
+
+    startDate = lastWeek.startDate;
+    endDate = thisWeek.endDate;
+  } else if (period === 'monthly') {
+    const thisMonth = getThisMonthStartAndEndDates();
+    const lastMonth = getLastMonthStartAndEndDates();
+
+    startDate = lastMonth.startDate;
+    endDate = thisMonth.endDate;
+  }
+
+  const data: unknown = await Expense.aggregate([
+    {
+      $match: {
+        user: new mongoose.Types.ObjectId(userId),
+        transactionDate: {
+          $lte: endDate,
+          $gte: startDate
+        }
+      }
+    },
+    {
+      $sort: {
+        createdAt: 1
+      }
+    },
+
+    {
+      $group: {
+        _id: {
+          $dateToString: { date: '$transactionDate' }
+        },
+        totalAmount: { $sum: '$amount' }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        transactionDate: { $dateFromString: { dateString: '$_id' } },
+        amount: '$totalAmount'
+      }
+    },
+    {
+      $sort: { transactionDate: 1 }
+    }
+  ]).exec();
+
+  return JSON.parse(JSON.stringify(data)) as {
+    amount: number;
+    transactionDate: Date;
+  }[];
+};
