f621de7ba084dab4fe2dd27effa6af36099a1f19
unknown <michaelxaviercanonizado@gmail.com>
2024-06-21 21:48:42 +0800

    chore: intialize <Chart.Label/>

--- src/components/sections/chart/index.tsx
+++ src/components/sections/chart/index.tsx
@@ -1,10 +1,12 @@
 import Area from './area/area';
+import Label from './label/label';
 import Line from './line';
 import Progress from './progress/progress';
 
 const Chart = {
   Line: Line,
   Progress: Progress,
-  Area: Area
+  Area: Area,
+  Label: Label
 };
 export default Chart;
---
+++ src/components/sections/chart/label/label.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Label = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className=''>
+      <p className=''>{children}</p>
+    </div>
+  );
+};
+
+export default Label;
