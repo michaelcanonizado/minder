09bec33924a97a3117b35a5a4129ed87ef39b1a5
unknown <michaelxaviercanonizado@gmail.com>
2024-06-22 18:52:15 +0800

    feat: render an area chart and use the net amount chart data to feed the area chart

--- src/app/(user)/dashboard/net-amount.tsx
+++ src/app/(user)/dashboard/net-amount.tsx
@@ -1,3 +1,5 @@
+import Chart from '@/components/sections/chart';
+import { formatChartDataDateProperties } from '@/helpers/format/format-chart-data-date-properties';
 import { getNetAmountChartData } from '@/lib/balance/get.net-amount-chart-data';
 
 const NetAmount = async () => {
@@ -5,9 +7,21 @@ const NetAmount = async () => {
 
   const data = await getNetAmountChartData(userId, 'monthly');
 
-  console.log(data);
+  const formattedData = formatChartDataDateProperties(data);
 
-  return <div>NetAmount</div>;
+  console.log(formattedData);
+
+  return (
+    <>
+      <Chart.Area data={formattedData} index='date' categories={['amount']} />
+      <div className='flex flex-row justify-between px-4 pb-4 pt-8'>
+        <Chart.Label>{formattedData[0].date}</Chart.Label>
+        <Chart.Label>
+          {formattedData[formattedData.length - 1].date}
+        </Chart.Label>
+      </div>
+    </>
+  );
 };
 
 export default NetAmount;
