4de77f820db75aacce293ad58de7206c4309f1b4
unknown <michaelxaviercanonizado@gmail.com>
2024-06-22 19:10:28 +0800

    docs: add comments

--- src/helpers/dates/get-this-year-start-and-end-dates.ts
+++ src/helpers/dates/get-this-year-start-and-end-dates.ts
@@ -1,6 +1,8 @@
 export const getThisYearStartAndEndDates = () => {
   const now = new Date();
+  // Start will be @ January 1, 12:00:00AM
   const startDate = new Date(now.getFullYear(), 0, 1, 0, 0, 0, 0);
+  // Start will be @ December 31, 11:59:99PM
   const endDate = new Date(now.getFullYear(), 11, 31, 23, 59, 59, 999);
 
   return {
