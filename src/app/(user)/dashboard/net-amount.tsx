4cbd2b446e16a321e530332ede85a9637ee5d1e1
unknown <michaelxaviercanonizado@gmail.com>
2024-06-23 21:53:42 +0800

    chore: remove area graph components and prepare route for ui ui rework

--- src/app/(user)/dashboard/net-amount.tsx
+++ src/app/(user)/dashboard/net-amount.tsx
@@ -1,25 +1,7 @@
-import Chart from '@/components/sections/chart';
-import { formatChartDataDateProperties } from '@/helpers/format/format-chart-data-date-properties';
-import { getNetAmountChartData } from '@/lib/balance/get.net-amount-chart-data';
-
 const NetAmount = async () => {
   const userId = process.env.TEMP_USER_ID!;
 
-  const data = await getNetAmountChartData(userId, 'yearly');
-
-  const formattedData = formatChartDataDateProperties(data);
-
-  return (
-    <>
-      <Chart.Area data={formattedData} index='date' categories={['amount']} />
-      <div className='flex flex-row justify-between px-4 pb-4 pt-8'>
-        <Chart.Label>{formattedData[0].date}</Chart.Label>
-        <Chart.Label>
-          {formattedData[formattedData.length - 1].date}
-        </Chart.Label>
-      </div>
-    </>
-  );
+  return <div className=''>Net Amount</div>;
 };
 
 export default NetAmount;
--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -9,10 +9,6 @@ import Bento from '@/components/sections/bento';
 import Chart from '@/components/sections/chart';
 
 import NetAmount from './net-amount';
-import WeeklyIncome from './weekly-income';
-import WeeklyExpense from './weekly-expense';
-import MonthlyIncome from './monthly-income';
-import MonthlyExpense from './monthly-expense';
 
 import { getBalanceData } from '@/lib/balance/get-balance-data';
 import { getPercentageChange } from '@/helpers/get-percentage-change';
@@ -35,160 +31,109 @@ type BalanceType = {
 const Dashboard = async () => {
   const userId = process.env.TEMP_USER_ID!;
 
-  const balanceData = await getBalanceData(userId);
+  // const balanceData = await getBalanceData(userId);
 
-  if (!balanceData) {
-    return <h1 className='text-display'>User was not found!</h1>;
-  }
+  // if (!balanceData) {
+  //   return <h1 className='text-display'>User was not found!</h1>;
+  // }
 
-  const netAmountPercentageChange = getPercentageChange(
-    balanceData.net.total.lastWeek,
-    balanceData.net.total.current
-  );
-  const incomeThisWeekPercentageChange = getPercentageChange(
-    balanceData.income.period.lastWeek,
-    balanceData.income.period.thisWeek
-  );
-  const expenseThisWeekPercentageChange = getPercentageChange(
-    balanceData.expense.period.lastWeek,
-    balanceData.expense.period.thisWeek
-  );
-  const incomeThisMonthPercentageChange = getPercentageChange(
-    balanceData.income.period.lastMonth,
-    balanceData.income.period.thisMonth
-  );
-  const expenseThisMonthPercentageChange = getPercentageChange(
-    balanceData.expense.period.lastMonth,
-    balanceData.expense.period.thisMonth
-  );
+  // const netAmountPercentageChange = getPercentageChange(
+  //   balanceData.net.total.lastWeek,
+  //   balanceData.net.total.current
+  // );
+  // const incomeThisWeekPercentageChange = getPercentageChange(
+  //   balanceData.income.period.lastWeek,
+  //   balanceData.income.period.thisWeek
+  // );
+  // const expenseThisWeekPercentageChange = getPercentageChange(
+  //   balanceData.expense.period.lastWeek,
+  //   balanceData.expense.period.thisWeek
+  // );
+  // const incomeThisMonthPercentageChange = getPercentageChange(
+  //   balanceData.income.period.lastMonth,
+  //   balanceData.income.period.thisMonth
+  // );
+  // const expenseThisMonthPercentageChange = getPercentageChange(
+  //   balanceData.expense.period.lastMonth,
+  //   balanceData.expense.period.thisMonth
+  // );
 
-  const balances: BalanceType[] = [
-    {
-      tabName: 'Net Amount',
-      header: 'Net Amount',
-      amount: balanceData.net.total.current,
-      percentageChange: {
-        percentage: netAmountPercentageChange,
-        isPositive: netAmountPercentageChange > 0 ? true : false,
-        difference:
-          balanceData.net.total.current - balanceData.net.total.lastWeek,
-        timePeriod: 'weekly'
-      },
-      graph: <NetAmount />
-    },
-    {
-      tabName: 'Income (weekly)',
-      header: 'Income this week',
-      amount: balanceData.income.period.thisWeek,
-      percentageChange: {
-        percentage: incomeThisWeekPercentageChange,
-        isPositive: incomeThisWeekPercentageChange > 0 ? true : false,
-        difference:
-          balanceData.income.period.thisWeek -
-          balanceData.income.period.lastWeek,
-        timePeriod: 'weekly'
-      },
-      graph: <WeeklyIncome />
-    },
-    {
-      tabName: 'Expense (weekly)',
-      header: 'Expenses this week',
-      amount: balanceData.expense.period.thisWeek,
-      percentageChange: {
-        percentage: expenseThisWeekPercentageChange,
-        isPositive: expenseThisWeekPercentageChange >= 0 ? false : true,
-        difference:
-          balanceData.expense.period.thisWeek -
-          balanceData.expense.period.lastWeek,
-        timePeriod: 'weekly'
-      },
-      graph: <WeeklyExpense />
-    },
-    {
-      tabName: 'Income (monthly)',
-      header: 'Income this month',
-      amount: balanceData.income.period.thisMonth,
-      percentageChange: {
-        percentage: incomeThisMonthPercentageChange,
-        isPositive: incomeThisMonthPercentageChange > 0 ? true : false,
-        difference:
-          balanceData.income.period.thisMonth -
-          balanceData.income.period.lastMonth,
-        timePeriod: 'monthly'
-      },
-      graph: <MonthlyIncome />
-    },
-    {
-      tabName: 'Expense (monthly)',
-      header: 'Expenses this month',
-      amount: balanceData.expense.period.thisMonth,
-      percentageChange: {
-        percentage: expenseThisMonthPercentageChange,
-        isPositive: expenseThisMonthPercentageChange >= 0 ? false : true,
-        difference:
-          balanceData.expense.period.thisMonth -
-          balanceData.expense.period.lastMonth,
-        timePeriod: 'monthly'
-      },
-      graph: <MonthlyExpense />
-    }
-  ];
+  // const balances: BalanceType[] = [
+  //   {
+  //     tabName: 'Net Amount',
+  //     header: 'Net Amount',
+  //     amount: balanceData.net.total.current,
+  //     percentageChange: {
+  //       percentage: netAmountPercentageChange,
+  //       isPositive: netAmountPercentageChange > 0 ? true : false,
+  //       difference:
+  //         balanceData.net.total.current - balanceData.net.total.lastWeek,
+  //       timePeriod: 'weekly'
+  //     },
+  //     graph: <NetAmount />
+  //   },
+  //   {
+  //     tabName: 'Income (weekly)',
+  //     header: 'Income this week',
+  //     amount: balanceData.income.period.thisWeek,
+  //     percentageChange: {
+  //       percentage: incomeThisWeekPercentageChange,
+  //       isPositive: incomeThisWeekPercentageChange > 0 ? true : false,
+  //       difference:
+  //         balanceData.income.period.thisWeek -
+  //         balanceData.income.period.lastWeek,
+  //       timePeriod: 'weekly'
+  //     },
+  //     graph: <WeeklyIncome />
+  //   },
+  //   {
+  //     tabName: 'Expense (weekly)',
+  //     header: 'Expenses this week',
+  //     amount: balanceData.expense.period.thisWeek,
+  //     percentageChange: {
+  //       percentage: expenseThisWeekPercentageChange,
+  //       isPositive: expenseThisWeekPercentageChange >= 0 ? false : true,
+  //       difference:
+  //         balanceData.expense.period.thisWeek -
+  //         balanceData.expense.period.lastWeek,
+  //       timePeriod: 'weekly'
+  //     },
+  //     graph: <WeeklyExpense />
+  //   },
+  //   {
+  //     tabName: 'Income (monthly)',
+  //     header: 'Income this month',
+  //     amount: balanceData.income.period.thisMonth,
+  //     percentageChange: {
+  //       percentage: incomeThisMonthPercentageChange,
+  //       isPositive: incomeThisMonthPercentageChange > 0 ? true : false,
+  //       difference:
+  //         balanceData.income.period.thisMonth -
+  //         balanceData.income.period.lastMonth,
+  //       timePeriod: 'monthly'
+  //     },
+  //     graph: <MonthlyIncome />
+  //   },
+  //   {
+  //     tabName: 'Expense (monthly)',
+  //     header: 'Expenses this month',
+  //     amount: balanceData.expense.period.thisMonth,
+  //     percentageChange: {
+  //       percentage: expenseThisMonthPercentageChange,
+  //       isPositive: expenseThisMonthPercentageChange >= 0 ? false : true,
+  //       difference:
+  //         balanceData.expense.period.thisMonth -
+  //         balanceData.expense.period.lastMonth,
+  //       timePeriod: 'monthly'
+  //     },
+  //     graph: <MonthlyExpense />
+  //   }
+  // ];
 
   return (
     <div className='px-8'>
       <DashboardContextProvider>
         <Bento className='grid-cols-1'>
-          <Bento.Box>
-            <Tabs defaultValue={balances[0].tabName} className='w-full'>
-              {balances.map(item => {
-                return (
-                  <TabsContent value={item.tabName}>
-                    <Bento.Box.Header className='border-none'>
-                      <Balance.Compact>
-                        <Balance.Compact.Header>
-                          {item.header}
-                        </Balance.Compact.Header>
-                        <Balance.Compact.Amount>
-                          {' '}
-                          $
-                          {item.amount.toLocaleString('en-US', {
-                            maximumFractionDigits: 2,
-                            minimumFractionDigits: 2
-                          })}
-                        </Balance.Compact.Amount>
-                        <Balance.Compact.SubHeader>
-                          <span
-                            className={`flex flex-row ${item.percentageChange.isPositive ? 'text-accent-100' : 'text-accent-200'}`}
-                          >
-                            {item.percentageChange.difference.toLocaleString(
-                              'en-US',
-                              {
-                                signDisplay: 'always'
-                              }
-                            )}
-                            &nbsp; (
-                            {item.percentageChange.percentage > 0 ? (
-                              <ArrowUp className='h-fit w-[14px]' />
-                            ) : (
-                              <ArrowDown className='h-fit w-[14px]' />
-                            )}
-                            {` ${item.percentageChange.percentage}%`})
-                          </span>
-                          &nbsp;
-                          {item.percentageChange.timePeriod === 'weekly'
-                            ? 'vs last week'
-                            : 'vs last month'}
-                        </Balance.Compact.SubHeader>
-                      </Balance.Compact>
-                    </Bento.Box.Header>
-                    <Bento.Box.Content className='p-0'>
-                      {item.graph}
-                    </Bento.Box.Content>
-                  </TabsContent>
-                );
-              })}
-            </Tabs>
-          </Bento.Box>
           <Bento.Box className=''>
             <Bento.Box.Header>
               <p className='text-heading-100'>Expense Budget</p>
