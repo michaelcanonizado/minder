773c3670833e82e7d8f7b8c2abccf0aeaf8fa63d
unknown <michaelxaviercanonizado@gmail.com>
2024-06-26 23:45:00 +0800

    feat: use fetched expense chart data to render the area cahrt

--- src/app/(user)/dashboard/Expense.tsx
+++ src/app/(user)/dashboard/Expense.tsx
@@ -13,7 +13,7 @@ import Bento from '@/components/sections/bento';
 import Chart from '@/components/sections/chart';
 import Balance from '@/components/sections/balance';
 
-const Income = () => {
+const Expense = () => {
   const userId = process.env.NEXT_PUBLIC_TEMP_USER_ID!;
 
   const { dashboard, changeDashboardPeriod } = useDashboardContext();
@@ -36,11 +36,86 @@ const Income = () => {
 
   console.log(data);
 
+  if (!data) {
+    return (
+      <Bento.Box>
+        <Bento.Box.Header>No Data</Bento.Box.Header>
+      </Bento.Box>
+    );
+  }
+
+  console.log('Expense component render');
+
+  const startDate = data ? data.rows[0].date : '';
+  const endDate = data ? data.rows[data.rows.length - 1].date : '';
+
+  let headerTitle = 'Expense';
+  if (dashboard.period === 'weekly') {
+    headerTitle = 'Expense this week';
+  } else if (dashboard.period === 'monthly') {
+    headerTitle = 'Expense this month';
+  } else if (dashboard.period === 'yearly') {
+    headerTitle = 'Expense this year';
+  }
+
+  const headerAmount = data.balance.amount.toLocaleString('en-US', {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2
+  });
+
+  const headerDifference =
+    data.balance.percentageChange.difference.toLocaleString('en-US', {
+      signDisplay: 'always'
+    });
+
+  const headerArrow =
+    data.balance.percentageChange.percentage > 0 ? (
+      <ArrowUp className='h-fit w-[14px]' />
+    ) : (
+      <ArrowDown className='h-fit w-[14px]' />
+    );
+
+  let headerLabel = '';
+  if (dashboard.period === 'weekly') {
+    headerLabel = 'vs last week';
+  } else if (dashboard.period === 'monthly') {
+    headerLabel = 'vs last month';
+  } else if (dashboard.period === 'yearly') {
+    headerLabel = 'vs last year';
+  }
+
+  const header = (
+    <Balance.Compact>
+      <Balance.Compact.Header>{headerTitle}</Balance.Compact.Header>
+      <Balance.Compact.Amount> ${headerAmount}</Balance.Compact.Amount>
+      <Balance.Compact.SubHeader>
+        <span
+          className={`flex flex-row ${data.balance.percentageChange.isPositive ? 'text-accent-100' : 'text-accent-200'}`}
+        >
+          {headerDifference}
+          &nbsp; ({headerArrow}
+          {` ${data.balance.percentageChange.percentage}%`})
+        </span>
+        &nbsp;
+        {headerLabel}
+      </Balance.Compact.SubHeader>
+    </Balance.Compact>
+  );
+
   return (
     <Bento.Box>
-      <Bento.Box.Header>No Data</Bento.Box.Header>
+      <Bento.Box.Header className='border-none'>{header}</Bento.Box.Header>
+      <Bento.Box.Content className='p-0'>
+        {data && (
+          <Chart.Area data={data.rows} index='date' categories={['amount']} />
+        )}
+        <div className='flex flex-row justify-between px-4 pb-4 pt-8'>
+          <Chart.Label>{startDate}</Chart.Label>
+          <Chart.Label>{endDate}</Chart.Label>
+        </div>
+      </Bento.Box.Content>
     </Bento.Box>
   );
 };
 
-export default Income;
+export default Expense;
