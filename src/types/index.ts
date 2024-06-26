84bcd8f8cb730041675dbf9d09bd76554feca79c
unknown <michaelxaviercanonizado@gmail.com>
2024-06-26 20:50:35 +0800

    feat: add ChartData

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,19 @@ export type LinkItem = {
 export type LinkItems = LinkItem[];
 
 export type Period = 'weekly' | 'monthly' | 'yearly';
+
+export type ChartRow = {
+  amount: number;
+  date: Date;
+};
+export type ChartData = {
+  balance: {
+    amount: number;
+    percentageChange: {
+      percentage: number;
+      difference: number;
+      isPositive: boolean;
+    };
+  };
+  rows: ChartRow[];
+};
