bbb4e158613b51d6522cff822c7e2c00441b9bc9
unknown <michaelxaviercanonizado@gmail.com>
2024-06-26 22:02:28 +0800

    feat: add chart header

--- src/app/(user)/dashboard/income.tsx
+++ src/app/(user)/dashboard/income.tsx
@@ -8,6 +8,9 @@ import { getIncomesChartData } from '@/lib/income/get-incomes-chart-data';
 import { ChartData, ChartRow } from '@/types';
 import { useEffect, useState } from 'react';
 
+import { ArrowDown, ArrowUp } from 'lucide-react';
+import Balance from '@/components/sections/balance';
+
 const Income = () => {
   const userId = process.env.NEXT_PUBLIC_TEMP_USER_ID!;
 
@@ -33,16 +36,75 @@ const Income = () => {
     getData();
   }, [dashboard]);
 
+  if (!data) {
+    return (
+      <Bento.Box>
+        <Bento.Box.Header>No Data</Bento.Box.Header>
+      </Bento.Box>
+    );
+  }
+
   console.log('Income component render');
 
   const startDate = data ? data.rows[0].date : '';
   const endDate = data ? data.rows[data.rows.length - 1].date : '';
 
+  let headerTitle = 'Income';
+  if (dashboard.period === 'weekly') {
+    headerTitle = 'Income this week';
+  } else if (dashboard.period === 'monthly') {
+    headerTitle = 'Income this month';
+  } else if (dashboard.period === 'yearly') {
+    headerTitle = 'Income this year';
+  }
+
+  const headerAmount = data.balance.amount.toLocaleString('en-US', {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2
+  });
+
+  const headerDifference =
+    data.balance.percentageChange.difference.toLocaleString('en-US', {
+      signDisplay: 'always'
+    });
+
+  const headerArrow =
+    data.balance.percentageChange.percentage > 0 ? (
+      <ArrowUp className='h-fit w-[14px]' />
+    ) : (
+      <ArrowDown className='h-fit w-[14px]' />
+    );
+
+  let headerLabel = '';
+  if (dashboard.period === 'weekly') {
+    headerLabel = 'vs last week';
+  } else if (dashboard.period === 'monthly') {
+    headerLabel = 'vs last month';
+  } else if (dashboard.period === 'yearly') {
+    headerLabel = 'vs last year';
+  }
+
+  const header = (
+    <Balance.Compact>
+      <Balance.Compact.Header>{headerTitle}</Balance.Compact.Header>
+      <Balance.Compact.Amount> ${headerAmount}</Balance.Compact.Amount>
+      <Balance.Compact.SubHeader>
+        <span
+          className={`flex flex-row ${data.balance.percentageChange.isPositive ? 'text-accent-100' : 'text-accent-200'}`}
+        >
+          {headerDifference}
+          &nbsp; ({headerArrow}
+          {` ${data.balance.percentageChange.percentage}%`})
+        </span>
+        &nbsp;
+        {headerLabel}
+      </Balance.Compact.SubHeader>
+    </Balance.Compact>
+  );
+
   return (
     <Bento.Box>
-      <Bento.Box.Header className='border-none'>
-        Income {dashboard.period}
-      </Bento.Box.Header>
+      <Bento.Box.Header className='border-none'>{header}</Bento.Box.Header>
       <Bento.Box.Content className='p-0'>
         {data && (
           <Chart.Area data={data.rows} index='date' categories={['amount']} />
