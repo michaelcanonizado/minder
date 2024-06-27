5d92652582177bae18566ddd4fb855e6ff9a10d3
unknown <michaelxaviercanonizado@gmail.com>
2024-06-27 15:55:06 +0800

    refactor: move default styles fo div container to allow the optional classes to overwrite the defaults

--- src/components/sections/balance/compact/compact.tsx
+++ src/components/sections/balance/compact/compact.tsx
@@ -2,71 +2,55 @@ import { cn } from '@/lib/utils';
 import { ArrowDown, ArrowUp } from 'lucide-react';
 import React from 'react';
 
-const Compact = ({ children }: { children?: React.ReactNode }) => {
-  const arrowIconClasses = 'w-[16px] h-fit';
+const Compact = ({
+  children,
+  className
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => {
+  return <div className=''>{children}</div>;
+};
 
-  // const BalancePercentageChange = () => {
-  //   return (
-  //     <p className='text-body-200 flex flex-row text-muted-foreground'>
-  //       <span
-  //         className={`flex flex-row ${percentageChange.isPositive ? 'text-accent-100' : 'text-accent-200'}`}
-  //       >
-  //         {percentageChange.isPositive ? '+' : '-'}
-  //         {percentageChange.difference}(
-  //         {percentageChange.isPositive ? (
-  //           <ArrowUp className={arrowIconClasses} />
-  //         ) : (
-  //           <ArrowDown className={arrowIconClasses} />
-  //         )}
-  //         {percentageChange.percentage}%)
-  //       </span>
-  //       &nbsp;vs last week
-  //     </p>
-  //   );
-  // };
-
+const Header = ({
+  children,
+  className
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className=''>
-      {/* <div className=''>
-        <p className='text-body-100 text-muted-foreground'>{title}</p>
-      </div>
-      <div className=''>
-        <h3 className='text-heading-100'>
-          $
-          {amount.toLocaleString('en-US', {
-            maximumFractionDigits: 2
-          })}
-        </h3>
-      </div>
-      <div className=''>
-        <BalancePercentageChange />
-      </div> */}
-      {children}
+    <div className='text-body-100 text-muted-foreground'>
+      <p className={cn('', className)}>{children}</p>
     </div>
   );
 };
 
-const Header = ({ children }: { children?: React.ReactNode }) => {
+const Amount = ({
+  children,
+  className
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className=''>
-      <p className='text-body-100 text-muted-foreground'>{children}</p>
+    <div className='text-heading-100'>
+      <h3 className={cn('', className)}>{children}</h3>
     </div>
   );
 };
 
-const Amount = ({ children }: { children?: React.ReactNode }) => {
+const SubHeader = ({
+  children,
+  className
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <div className=''>
-      <h3 className='text-heading-100'>{children}</h3>
+    <div className='text-body-200 mt-1  text-muted-foreground'>
+      <p className={cn('flex flex-row', className)}>{children}</p>
     </div>
-  );
-};
-
-const SubHeader = ({ children }: { children?: React.ReactNode }) => {
-  return (
-    <p className={'text-body-200 mt-1 flex flex-row text-muted-foreground'}>
-      {children}
-    </p>
   );
 };
 
