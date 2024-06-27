749f44d4bbac7a35bc8808042a5161dae45c6963
unknown <michaelxaviercanonizado@gmail.com>
2024-06-27 13:58:55 +0800

    feat: add getLastYearStartAndEndDates() helper function

---
+++ src/helpers/dates/get-last-year-start-and-end-dates.ts
@@ -0,0 +1,14 @@
+import { PeriodDates } from '@/types';
+
+export const getLastYearStartAndEndDates = (): PeriodDates => {
+  const now = new Date();
+  // Start will be @ January 1, 12:00:00AM
+  const startDate = new Date(now.getFullYear() - 1, 0, 1, 0, 0, 0, 0);
+  // Start will be @ December 31, 11:59:99PM
+  const endDate = new Date(now.getFullYear() - 1, 11, 31, 23, 59, 59, 999);
+
+  return {
+    startDate: startDate,
+    endDate: endDate
+  };
+};
