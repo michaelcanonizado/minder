35ee5da6af86ad0d359718f46457a02465db8c36
unknown <michaelxaviercanonizado@gmail.com>
2024-06-28 22:43:49 +0800

    docs: add comments and tags

--- src/helpers/format/format-currency.ts
+++ src/helpers/format/format-currency.ts
@@ -1,5 +1,11 @@
 import { UserCurrencyType } from '@/models/user';
 
+/**
+ * Adds currency code/symbol to a number
+ *
+ * @param value number to be formatted
+ * @returns the number with the currency code
+ */
 export const formatCurrency = (value: number) => {
   if (!value) {
     return '';
