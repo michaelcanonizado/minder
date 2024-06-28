4cdaf14129cd032be3e41834c2d0538886634405
unknown <michaelxaviercanonizado@gmail.com>
2024-06-28 23:04:50 +0800

    docs: add comments and tags

--- src/helpers/dates/get-this-year-start-and-end-dates.ts
+++ src/helpers/dates/get-this-year-start-and-end-dates.ts
@@ -1,10 +1,16 @@
 import { PeriodDates } from '@/types';
 
+/**
+ * Gets the start and end dates of this year
+ *
+ * @returns start(Jan 1, 12:00:000AM) and end(Dec 31,
+ * 11:59:999PM) dates
+ */
 export const getThisYearStartAndEndDates = (): PeriodDates => {
   const now = new Date();
-  // Start will be @ January 1, 12:00:00AM
+
   const startDate = new Date(now.getFullYear(), 0, 1, 0, 0, 0, 0);
-  // Start will be @ December 31, 11:59:99PM
+
   const endDate = new Date(now.getFullYear(), 11, 31, 23, 59, 59, 999);
 
   return {
