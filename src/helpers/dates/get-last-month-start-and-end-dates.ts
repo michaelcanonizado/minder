77377934df6ebc4d5acb0f92b6589b32bbbdf7d8
unknown <michaelxaviercanonizado@gmail.com>
2024-06-28 22:49:17 +0800

    docs: add comments and tags

--- src/helpers/dates/get-last-month-start-and-end-dates.ts
+++ src/helpers/dates/get-last-month-start-and-end-dates.ts
@@ -1,18 +1,23 @@
 import { PeriodDates } from '@/types';
 
+/**
+ * Gets the start and end date of last month
+ *
+ * @returns start and end date
+ */
 export const getLastMonthStartAndEndDates = (): PeriodDates => {
   const now = new Date();
   const year = now.getFullYear();
   const month = now.getMonth();
 
-  // Calculate the first day of the previous month
+  /* Calculate the first day of the previous month */
   const firstDayOfLastMonth = new Date(year, month - 1, 1);
-  // Set time to 12:00 AM
+  /* Set time to 12:00:00AM */
   firstDayOfLastMonth.setHours(0, 0, 0, 0);
 
-  // Calculate the last day of the previous month
+  /* Calculate the last day of the previous month */
   const lastDayOfLastMonth = new Date(year, month, 0);
-  // Set time to 11:59 PM
+  /* Set time to 11:59:999PM */
   lastDayOfLastMonth.setHours(23, 59, 59, 999);
 
   return {
