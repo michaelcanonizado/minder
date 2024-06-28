905ccca574000a2dae04d97fb75d1ae11b1b8454
unknown <michaelxaviercanonizado@gmail.com>
2024-06-28 23:21:36 +0800

    docs: add comments and tags

--- src/seeds/expense.ts
+++ src/seeds/expense.ts
@@ -3,13 +3,25 @@ import { databaseConnect, databaseClose } from '@/helpers/database/database';
 import Expense from '@/models/expense';
 import User from '@/models/user';
 
-// WARNING
-// This function does not automatically call seedIncome (npm run seed-income), where the income collection is cleared, new incomes are made, and the wallets are updated. Therefore, calling this function multiple times can cause an overflow and mismatch between the expenses and incomes
-// To avoid this, always run 'npm run seed-income' before running this file(npm run seed-expense), or seedIncome can be imported from ./income.ts and be called before the function below runs.
+/**
+ * Seeds expense collection with random values
+ *
+ * WARNING
+ * This function does not automatically call seedIncome (npm run
+ *  seed-income), where the income collection is cleared, new
+ * incomes are made, and the wallets are updated. Therefore,
+ * calling this function multiple times can cause an overflow and
+ * mismatch between the expenses and incomes
+ *
+ * To avoid this, always run 'npm run seed-income' before running
+ * this file(npm run seed-expense), or seedIncome can be imported
+ * from ./income.ts and be called before the function below runs.
+ *
+ * @returns void
+ */
 const seedExpense = async () => {
   databaseConnect();
 
-  // Get user
   const user = await User.findOne({ profile: { username: 'mikey' } });
   if (!user) {
     console.log('User not found!');
@@ -17,7 +29,7 @@ const seedExpense = async () => {
   }
   console.log('User found!');
 
-  // Delete contents of Expense collection
+  /* Delete contents of Expense collection */
   await Expense.deleteMany({});
   console.log('Deleted all expenses!');
 
@@ -33,13 +45,14 @@ const seedExpense = async () => {
   const numOfWallets = user.wallets.length;
   const numOfCategories = user.categories ? user.categories?.income.length : 0;
 
-  // Seed 3-15 expense logs for each wallet
+  /* Seed 3-15 expense transactions for each wallet */
   for (let i = 0; i < numOfWallets; i++) {
     const randNumofExpenses = Math.floor(Math.random() * (15 - 3 + 1) + 3);
 
     for (let j = 0; j < randNumofExpenses; j++) {
       let randAmount =
         Math.floor(
+          /* @ts-ignore */
           Math.random() * (Math.floor(user.wallets[i].balance / 3) - 100 + 1)
         ) + 100;
       const randCategory = Math.floor(Math.random() * numOfCategories);
@@ -62,18 +75,19 @@ const seedExpense = async () => {
 
       for (const wallet of user.wallets) {
         if (wallet._id == user.wallets[i]._id) {
+          /* @ts-ignore */
           wallet.balance -= randAmount;
         }
       }
     }
   }
 
-  // Push data to database
+  /* Push data to database */
   const expenses = await Expense.insertMany(expenseLogs);
   console.log('Expenses generated:');
   console.log(expenses);
 
-  // Update user's balances
+  /* Update user's balances */
   user.balance.netBalance -= totalAmount;
   user.balance.totalExpense = totalAmount;
   await user.save();
