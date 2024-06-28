8c3cab41d1873936103730f527e2f642fb54a300
unknown <michaelxaviercanonizado@gmail.com>
2024-06-28 22:41:58 +0800

    docs: add comments and tags

--- src/helpers/get-percentage-change.ts
+++ src/helpers/get-percentage-change.ts
@@ -1,3 +1,10 @@
+/**
+ * Gets the percentage change between two values
+ *
+ * @param initialValue Starting Value
+ * @param finalValue End Value
+ * @returns the percentage change
+ */
 export const getPercentageChange = (
   initialValue: number,
   finalValue: number
