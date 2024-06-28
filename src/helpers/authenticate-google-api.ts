8ec428b694917e4f8d6c1aa9442643dcf6a145b3
unknown <michaelxaviercanonizado@gmail.com>
2024-06-28 22:40:43 +0800

    docs: add comments and tags

--- src/helpers/authenticate-google-api.ts
+++ src/helpers/authenticate-google-api.ts
@@ -1,22 +1,28 @@
 import { google } from 'googleapis';
 
+/**
+ * Authenticates access to Google Sheets with
+ * Google API
+ *
+ * @returns instance of googlesheets and auth object
+ */
 export const authenticateGoogleAPI = async () => {
   const keyFilePath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-  // Authenticate
+  /* Authenticate */
   const auth = new google.auth.GoogleAuth({
     keyFile: keyFilePath,
     scopes: 'https://www.googleapis.com/auth/spreadsheets'
   });
 
-  // Create client instance
+  /* Create client instance */
   const client = await auth.getClient();
 
-  // Create instance of Google Sheets API
-  // @ts-ignore
+  /* Create instance of Google Sheets API */
+  /* @ts-ignore - Somehow throws a type error */
   const googleSheets = google.sheets({ version: 'v4', auth: client });
 
-  // Throw error if an error occurred when authenticating
+  /* Throw error if an error occurred when authenticating */
   if (!googleSheets) {
     throw new Error('Error connecting with google API');
   }
