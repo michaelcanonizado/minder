da750532bd3aee358cbd2eb4f4341707035d735d
unknown <michaelxaviercanonizado@gmail.com>
2024-06-29 22:48:31 +0800

    chore: add dashboard context to <Categories/>

--- src/app/(user)/dashboard/categories.tsx
+++ src/app/(user)/dashboard/categories.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { useDashboardContext } from '@/context/dashboard-context';
+
 import { cn } from '@/lib/utils';
 
 import Bento from '@/components/sections/bento';
 import Chart from '@/components/sections/chart';
 
 const Categories = ({ className }: { className?: string }) => {
+  const userId = process.env.NEXT_PUBLIC_TEMP_USER_ID!;
+
+  const { dashboard, changeDashboardPeriod } = useDashboardContext();
+
+  console.log('Categories: ', dashboard);
+
   return (
     <Bento.Box className={cn('', className)}>
       <Bento.Box.Header>
