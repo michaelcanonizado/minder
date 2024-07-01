c23a6c8c3b09f3a499737e31ab6654277c13bed2
unknown <michaelxaviercanonizado@gmail.com>
2024-07-01 15:30:42 +0800

    feat: add color to each category

--- src/seeds/user.ts
+++ src/seeds/user.ts
@@ -9,6 +9,79 @@ import User from '@/models/user';
  *
  * @returns void
  */
+const colors = [
+  '#ff0000',
+  '#ff8700',
+  '#ffd300',
+  '#deff0a',
+  '#a1ff0a',
+  '#0aff99',
+  '#147df5',
+  '#580aff',
+  '#be0aff',
+  '#0aefff'
+];
+const expenses = [
+  {
+    name: 'Food',
+    color: '#ff0000'
+  },
+  {
+    name: 'Transportation',
+    color: '#ff8700'
+  },
+  {
+    name: 'Shopping',
+    color: '#ffd300'
+  },
+  {
+    name: 'Phone',
+    color: '#deff0a'
+  },
+  {
+    name: 'Housing',
+    color: '#a1ff0a'
+  },
+  {
+    name: 'Education',
+    color: '#0aff99'
+  },
+  {
+    name: 'Entertainment',
+    color: '#580aff'
+  },
+  {
+    name: 'Other',
+    color: '#be0aff'
+  },
+  {
+    name: 'None',
+    color: '#0aefff'
+  }
+];
+const incomes = [
+  {
+    name: 'Salary',
+    color: '#ff0000'
+  },
+  {
+    name: 'Allowance',
+    color: '#ff8700'
+  },
+  {
+    name: 'Investments',
+    color: '#0aefff'
+  },
+  {
+    name: 'Business',
+    color: '#a1ff0a'
+  },
+  {
+    name: 'Other',
+    color: '#580aff'
+  }
+];
+
 const seedUser = async () => {
   databaseConnect();
 
@@ -24,52 +97,8 @@ const seedUser = async () => {
       name: 'Philippine Peso'
     },
     categories: {
-      expense: [
-        {
-          name: 'Food'
-        },
-        {
-          name: 'Transportation'
-        },
-        {
-          name: 'Shopping'
-        },
-        {
-          name: 'Phone'
-        },
-        {
-          name: 'Housing'
-        },
-        {
-          name: 'Education'
-        },
-        {
-          name: 'Entertainment'
-        },
-        {
-          name: 'Other'
-        },
-        {
-          name: 'None'
-        }
-      ],
-      income: [
-        {
-          name: 'Salary'
-        },
-        {
-          name: 'Allowance'
-        },
-        {
-          name: 'Investments'
-        },
-        {
-          name: 'Business'
-        },
-        {
-          name: 'Other'
-        }
-      ]
+      expense: expenses,
+      income: incomes
     },
     wallets: [
       {
