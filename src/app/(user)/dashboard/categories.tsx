9ed2aeb4394287888a86336962ee5761f70c9460
unknown <michaelxaviercanonizado@gmail.com>
2024-07-03 23:22:22 +0800

    refactor: use data table instead of regular table

--- src/app/(user)/dashboard/categories.tsx
+++ src/app/(user)/dashboard/categories.tsx
@@ -6,22 +6,16 @@ import { useDashboardContext } from '@/context/dashboard-context';
 
 import { cn } from '@/lib/utils';
 import { getCategoriesChartData } from '@/lib/category/get-categories-chart-data';
+import { columns } from './_columns/categories';
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from '@/components/ui/table';
 import Bento from '@/components/sections/bento';
 import Chart from '@/components/sections/chart';
+import { default as DataTable } from '@/components/sections/table';
 
 const Categories = ({ className }: { className?: string }) => {
   const userId = process.env.NEXT_PUBLIC_TEMP_USER_ID!;
 
-  const { dashboard, changeDashboardPeriod } = useDashboardContext();
+  const { dashboard } = useDashboardContext();
 
   const [data, setData] = useState<Awaited<
     ReturnType<typeof getCategoriesChartData>
@@ -53,15 +47,6 @@ const Categories = ({ className }: { className?: string }) => {
     };
   });
 
-  const totalIncomeTransactionsCount = data.income!.reduce((sum, item) => {
-    return sum + item.rows.length;
-  }, 0);
-  const totalExpenseTransactionsCount = data.expense!.reduce((sum, item) => {
-    return sum + item.rows.length;
-  }, 0);
-
-  console.log(expenseCategoriesChartData);
-
   return (
     <Bento.Box className={cn('', className)}>
       <Bento.Box.Header>
@@ -75,56 +60,7 @@ const Categories = ({ className }: { className?: string }) => {
 
         <Chart.Progress.Stacked items={incomeCategoriesChartData} />
 
-        <Table className='mt-4'>
-          <TableHeader>
-            <TableRow className='text-body-200 w-full capitalize'>
-              <TableHead className=''>Category</TableHead>
-              <TableHead className='w-[100px]'>% of total</TableHead>
-              <TableHead className='w-[100px] max-w-max text-right'>
-                Amount
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data.income?.map(item => {
-              const amount = item.amount.toLocaleString('en-US', {
-                maximumFractionDigits: 2,
-                minimumFractionDigits: 0
-              });
-
-              const percentage = totalIncomeTransactionsCount
-                ? (
-                    (item.rows.length / totalIncomeTransactionsCount) *
-                    100
-                  ).toFixed(2)
-                : 0;
-
-              const name = item.name;
-              const color = item.color;
-
-              return (
-                <TableRow className='[&>*]:py-2' key={item._id}>
-                  <TableCell>
-                    <p
-                      className='text-body-200 w-fit rounded-full bg-accent/50 px-2 py-1 capitalize'
-                      style={{
-                        color: color
-                      }}
-                    >
-                      {name}
-                    </p>
-                  </TableCell>
-                  <TableCell className='text-body-200 text-right  text-muted-foreground'>
-                    {percentage}%
-                  </TableCell>
-                  <TableCell className='text-body-200 text-right text-muted-foreground'>
-                    ${amount}
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+        <DataTable.DataTable.Scroll columns={columns} data={data.income} />
       </Bento.Box.Content>
 
       <Bento.Box.Content className='flex flex-col'>
@@ -134,56 +70,7 @@ const Categories = ({ className }: { className?: string }) => {
 
         <Chart.Progress.Stacked items={expenseCategoriesChartData} />
 
-        <Table className='mt-4'>
-          <TableHeader>
-            <TableRow className='text-body-200 w-full capitalize'>
-              <TableHead className=''>Category</TableHead>
-              <TableHead className='w-[100px]'>% of total</TableHead>
-              <TableHead className='w-[100px] max-w-max text-right'>
-                Amount
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data.expense?.map(item => {
-              const amount = item.amount.toLocaleString('en-US', {
-                maximumFractionDigits: 2,
-                minimumFractionDigits: 0
-              });
-
-              const percentage = totalExpenseTransactionsCount
-                ? (
-                    (item.rows.length / totalExpenseTransactionsCount) *
-                    100
-                  ).toFixed(2)
-                : 0;
-
-              const name = item.name;
-              const color = item.color;
-
-              return (
-                <TableRow className='[&>*]:py-2' key={item._id}>
-                  <TableCell>
-                    <p
-                      className='text-body-200 w-fit rounded-full bg-accent/50 px-2 py-1 capitalize'
-                      style={{
-                        color: color
-                      }}
-                    >
-                      {name}
-                    </p>
-                  </TableCell>
-                  <TableCell className='text-body-200 text-right  text-muted-foreground'>
-                    {percentage}%
-                  </TableCell>
-                  <TableCell className='text-body-200 text-right text-muted-foreground'>
-                    ${amount}
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+        <DataTable.DataTable.Scroll columns={columns} data={data.expense} />
       </Bento.Box.Content>
     </Bento.Box>
   );
