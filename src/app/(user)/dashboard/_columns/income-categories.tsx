c0a6ce9362c2583110cf69f82498001ba28d711e
unknown <michaelxaviercanonizado@gmail.com>
2024-07-03 23:11:23 +0800

    feat: add data table column definition

---
+++ src/app/(user)/dashboard/_columns/income-categories.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+import { IncomeType } from '@/models/income';
+
+export const columns: ColumnDef<IncomeType>[] = [
+  {
+    accessorKey: 'name',
+    header: () => <div className=''>Category</div>,
+    cell: ({ row }) => {
+      return <div className='Capitalize'>{row.getValue('name')}</div>;
+    }
+  },
+  {
+    accessorKey: 'percentage',
+    header: () => <div className='text-right'>% Of Total</div>,
+    cell: ({ row }) => {
+      const percentage = parseFloat(row.getValue('percentage')).toFixed(2);
+      return <div className='text-right'>{percentage}</div>;
+    }
+  },
+  {
+    accessorKey: 'amount',
+    header: () => <div className='text-right'>Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue('amount'));
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(amount);
+
+      return <div className='text-right'>{formatted}</div>;
+    }
+  }
+];
