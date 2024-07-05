e932343b1b9fcdda3b5cc027efcadf491b2a322a
unknown <michaelxaviercanonizado@gmail.com>
2024-07-05 22:40:47 +0800

    feat: validate data coming from client using deleteCategoriesSchema

--- src/lib/category/delete-categories.ts
+++ src/lib/category/delete-categories.ts
@@ -1,5 +1,11 @@
 'use server';
 
+import deleteCategoriesSchema from '@/schemas/delete-categories';
+
 export const deleteCateogries = async (data: unknown) => {
+  const result = deleteCategoriesSchema.safeParse(data);
+
+  console.log(result);
+
   return;
 };
