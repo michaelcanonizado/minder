cc0d30a01ffddd88bdf8c0dab0733d082d417377
unknown <michaelxaviercanonizado@gmail.com>
2024-07-05 16:07:33 +0800

    feat: add deleteCategorySchema

---
+++ src/schemas/delete-categories.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const deleteCategoriesSchema = z.object({
+  categoryId: z
+    .string({ message: 'Please name the wallet' })
+    .min(1, { message: 'Please name the wallet' }),
+
+  userId: z
+    .string({ message: 'Error! Please try again.' })
+    .min(1, { message: 'Error! Please try again.' }),
+  formPath: z
+    .string({ message: 'Error! Please try again.' })
+    .min(1, { message: 'Error! Please try again.' })
+});
+
+export default deleteCategoriesSchema;
