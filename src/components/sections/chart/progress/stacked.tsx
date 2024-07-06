1d5c8d464c00f2aee99afd799d0f0218547c3eec
unknown <michaelxaviercanonizado@gmail.com>
2024-07-07 00:46:23 +0800

    feat: add tooltip to <Chart.Progress.Stacked/>

--- src/components/sections/chart/progress/stacked.tsx
+++ src/components/sections/chart/progress/stacked.tsx
@@ -18,7 +18,7 @@ const Stacked = ({ items, className }: StackedType) => {
   return (
     <div
       className={cn(
-        'flex h-[8px] w-full flex-row overflow-hidden rounded-full bg-muted',
+        'flex h-[20px] w-full flex-row rounded-full bg-muted',
         className
       )}
     >
@@ -26,13 +26,28 @@ const Stacked = ({ items, className }: StackedType) => {
         ? items.map((item, index) => {
             return (
               <div
-                className={cn('')}
+                className={cn('group relative cursor-pointer')}
                 style={{
                   width: `${(item.amount / totalAmount) * 100}%`,
                   backgroundColor: item.color
                 }}
                 key={index}
-              ></div>
+              >
+                <div
+                  className='left-[50%] z-50 hidden translate-x-[-50%] translate-y-[-140%] rounded-full px-4 py-1 group-hover:absolute group-hover:flex'
+                  style={{
+                    backgroundColor: item.color
+                  }}
+                >
+                  <div
+                    className='absolute left-[50%] z-10 size-[12px] translate-x-[-50%] translate-y-[150%] rotate-45 '
+                    style={{
+                      backgroundColor: item.color
+                    }}
+                  ></div>
+                  Food
+                </div>
+              </div>
             );
           })
         : ''}
