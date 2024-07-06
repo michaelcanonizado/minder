d2cf3f676f86eac22c5e8c6183dc8237859d9151
unknown <michaelxaviercanonizado@gmail.com>
2024-07-07 00:00:00 +0800

    chore: move category colors to @types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -41,12 +41,90 @@ export type ChartData = {
   rows: ChartRow[];
 };
 
+export type CategoryColor = {
+  name: string;
+  code: {
+    primary: string;
+    secondary: string;
+  };
+};
+export const categoryColors: CategoryColor[] = [
+  {
+    name: 'Red',
+    code: {
+      primary: '#d90429',
+      secondary: 'rgba(217, 4, 41, 0.1)'
+    }
+  },
+  {
+    name: 'Orange',
+    code: {
+      primary: '#faa307',
+      secondary: 'rgba(241, 135, 1, 0.1)'
+    }
+  },
+  {
+    name: 'Yellow',
+    code: {
+      primary: '#ffe600',
+      secondary: 'rgba(255, 230, 0, 0.1)'
+    }
+  },
+  {
+    name: 'Green',
+    code: {
+      primary: '#38b000',
+      secondary: 'rgba(56, 176, 0, 0.1)'
+    }
+  },
+  {
+    name: 'Blue',
+    code: {
+      primary: '#00a6fb',
+      secondary: 'rgba(0, 166, 251, 0.1)'
+    }
+  },
+  {
+    name: 'Purple',
+    code: {
+      primary: '#8900f2',
+      secondary: 'rgba(137, 0, 242, 0.1)'
+    }
+  },
+  {
+    name: 'Brown',
+    code: {
+      primary: '#b76935',
+      secondary: 'rgba(232, 93, 4, 0.1)'
+    }
+  },
+  {
+    name: 'Pink',
+    code: {
+      primary: '#ff5d8f',
+      secondary: 'rgba(255, 93, 143, 0.1)'
+    }
+  },
+  {
+    name: 'Gray',
+    code: {
+      primary: '#adb5bd',
+      secondary: 'rgba(173, 181, 189, 0.1)'
+    }
+  },
+  {
+    name: 'Turquoise',
+    code: {
+      primary: '#02c39a',
+      secondary: 'rgba(2, 195, 154, 0.1)'
+    }
+  }
+];
 export type CategoryType = 'income' | 'expense';
-
 export type CategoryChartData = {
   _id: string;
   name: string;
-  color: string;
+  color: CategoryColor;
   amount: number;
   rows: ExpenseType[] | IncomeType[];
   percentage: number;
