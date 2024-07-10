03671a4b8608bedb7292ef1034380e6d874501ba
unknown <michaelxaviercanonizado@gmail.com>
2024-07-10 23:23:38 +0800

    feat: add editCategorySchema

---
+++ src/schemas/edit-category.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+const editCategorySchema = z.object({
+  name: z
+    .string({ message: 'Please name the category' })
+    .min(1, { message: 'Please name the category' }),
+  colorId: z
+    .string({ message: 'Please choose a color' })
+    .min(1, { message: 'Please choose a color' }),
+
+  userId: z
+    .string({ message: 'Error! Please try again.' })
+    .min(1, { message: 'Error! Please try again.' }),
+  formPath: z
+    .string({ message: 'Error! Please try again.' })
+    .min(1, { message: 'Error! Please try again.' })
+});
+
+export default editCategorySchema;
