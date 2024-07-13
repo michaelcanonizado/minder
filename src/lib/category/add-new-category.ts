deaff7e78a0cc6622b8c455b827d978962354153
unknown <michaelxaviercanonizado@gmail.com>
2024-07-13 18:24:07 +0800

    feat: add ServerResponse return type and modify response messages to be displayed in toast

--- src/lib/category/add-new-category.ts
+++ src/lib/category/add-new-category.ts
@@ -1,18 +1,23 @@
 'use server';
 
-import User from '@/models/user';
 import { databaseConnect } from '@/helpers/database/database';
-
+import User from '@/models/user';
 import addCategorySchema from '@/schemas/add-cetegory';
-import { CategoryType, categoryColors } from '@/types';
+import { CategoryType, ServerResponse, categoryColors } from '@/types';
 import { revalidatePath } from 'next/cache';
 
-export const addNewCategory = async (data: unknown, type: CategoryType) => {
+export const addNewCategory = async (
+  data: unknown,
+  type: CategoryType
+): Promise<ServerResponse> => {
   const result = addCategorySchema.safeParse(data);
   if (!result.success) {
     return {
       isSuccessful: false,
-      message: 'Failed to add category! Please try again'
+      message: {
+        title: 'Error!',
+        description: 'Failed to add category! Please try again'
+      }
     };
   }
 
@@ -21,21 +26,28 @@ export const addNewCategory = async (data: unknown, type: CategoryType) => {
       return color;
     }
   });
-
   if (!selectedColor) {
     return {
       isSuccessful: false,
-      message: 'Failed to add category! Please try again'
+      message: {
+        title: 'Error!',
+        description: 'Failed to add category! Please try again'
+      }
     };
   }
 
   await databaseConnect();
 
   const userId = process.env.TEMP_USER_ID;
-
   const user = await User.findById(userId);
   if (!user) {
-    throw new Error('User not found!');
+    return {
+      isSuccessful: false,
+      message: {
+        title: 'Error!',
+        description: 'Failed to add category! Please try again'
+      }
+    };
   }
 
   if (type === 'income') {
@@ -64,6 +76,9 @@ export const addNewCategory = async (data: unknown, type: CategoryType) => {
 
   return {
     isSuccessful: true,
-    message: 'Successfully added category!'
+    message: {
+      title: 'Success!',
+      description: 'Successfully added category'
+    }
   };
 };
