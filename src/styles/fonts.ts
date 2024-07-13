1805139204fb34ae9be454f95c086b6bb178ee51
unknown <michaelxaviercanonizado@gmail.com>
2024-07-13 15:13:11 +0800

    fix: change font subset to fix font not applying

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -2,6 +2,6 @@ import { Poppins as FontSans } from 'next/font/google';
 
 export const fontSans = FontSans({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
+  subsets: ['latin-ext'],
   variable: '--font-sans'
 });
