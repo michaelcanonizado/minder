c8093cfc13bcd82db86ab9a0769def92c48a0285
unknown <michaelxaviercanonizado@gmail.com>
2024-07-16 17:48:37 +0800

    feat: add row actions

--- src/app/(user)/income/transactions/columns.tsx
+++ src/app/(user)/income/transactions/columns.tsx
@@ -6,6 +6,16 @@ import { IncomeType } from '@/models/income';
 import { UserWalletType } from '@/models/user';
 
 import { Checkbox } from '@/components/ui/checkbox';
+import { MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 
 export const columns: ColumnDef<IncomeType>[] = [
   {
@@ -84,5 +94,27 @@ export const columns: ColumnDef<IncomeType>[] = [
   {
     accessorKey: 'description',
     header: 'Description'
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    }
   }
 ];
