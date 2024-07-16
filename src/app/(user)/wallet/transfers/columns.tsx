eb902822eaa4d6e35bc8257b1feee18cc212a015
unknown <michaelxaviercanonizado@gmail.com>
2024-07-16 20:52:54 +0800

    feat: add row actions

--- src/app/(user)/wallet/transfers/columns.tsx
+++ src/app/(user)/wallet/transfers/columns.tsx
@@ -4,7 +4,18 @@ import { ColumnDef } from '@tanstack/react-table';
 import { format } from 'date-fns';
 import { BalanceTransferType } from '@/models/balance-transfer';
 import { UserWalletType } from '@/models/user';
+
 import { Checkbox } from '@/components/ui/checkbox';
+import { MoreHorizontal } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 
 export const columns: ColumnDef<BalanceTransferType>[] = [
   {
@@ -81,6 +92,26 @@ export const columns: ColumnDef<BalanceTransferType>[] = [
         <div className='min-w-[400px] max-w-[600px]'>
           <p className='text-wrap'>{description}</p>
         </div>
+      );
+    }
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       );
     }
   }
