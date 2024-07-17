7d26cf509732946d9a5b5b3136a4319017ae6da3
unknown <michaelxaviercanonizado@gmail.com>
2024-07-17 23:51:16 +0800

    fix: implement the form trigger directly in the table row instead of inside a dropwdown menu. There's a bug that prevents the inputs from being selected if Form which uses a dialog is inside of a dropdown

--- src/app/(user)/income/transactions/columns.tsx
+++ src/app/(user)/income/transactions/columns.tsx
@@ -6,16 +6,6 @@ import { IncomeType } from '@/models/income';
 import { UserWalletType } from '@/models/user';
 
 import { Checkbox } from '@/components/ui/checkbox';
-import { MoreHorizontal } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu';
 import Form from '@/components/sections/form';
 
 export const columns: ColumnDef<IncomeType>[] = [
@@ -110,21 +100,7 @@ export const columns: ColumnDef<IncomeType>[] = [
     cell: ({ row }) => {
       const rowData = row.original;
 
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='h-8 w-8 p-0'>
-              <span className='sr-only'>Open menu</span>
-              <MoreHorizontal className='h-4 w-4' />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align='end'>
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <Form.Edit.Income defaultValues={rowData} />
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
+      return <Form.Edit.Income defaultValues={rowData} />;
     }
   }
 ];
